9d896b176c178012a5d8630fe4f14ae0b29c0ef1
Sudherson Jeyakodi <sudherson@pearlthoughts.com>
2021-06-10 15:30:46 +0530

    removes duplication declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
-import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
-import { SpotlightComponent } from './components/spotlight/spotlight.component';
-import { ServiceComponent } from './components/service/service.component';
-import { AwardsComponent } from './components/awards/awards.component';
 
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
 @NgModule({
-  declarations: [AppComponent, LayoutComponent, SpotlightComponent, ServiceComponent, AwardsComponent],
+  declarations: [AppComponent, LayoutComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
