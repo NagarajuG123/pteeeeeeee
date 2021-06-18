1ca6abf8eb423dbefa66bc4dcb5c60dde956091a
poonkodi <poonkodi@pearlthoughts.com>
2021-06-18 14:56:48 +0530

    fix console issue.

--- src/app/components/spotlight/spotlight.component.ts
+++ src/app/components/spotlight/spotlight.component.ts
@@ -27,35 +27,30 @@ export class SpotlightComponent implements OnInit {
   getPeopleSpotlight() {
     this.apiService.getPeopleSpotlight().subscribe((response) => {
       this.peopleData = response;
-      console.log(this.peopleData);
     });
   }
 
   getFranchiseeSpotlight() {
     this.apiService.getFranchiseeSpotlight().subscribe((response) => {
       this.franchiseeData = response;
-      console.log(this.franchiseeData);
     });
   }
 
   getFranchisorSpotlight() {
     this.apiService.getFranchisorSpotlight().subscribe((response) => {
       this.franchisorData = response;
-      console.log(this.franchisorData);
     });
   }
 
   getIndustrySpotlight() {
     this.apiService.getIndustrySpotlight().subscribe((response) => {
       this.industryData = response;
-      console.log(this.industryData);
     });
   }
 
   getColumnSpotlight() {
     this.apiService.getColumnSpotlight().subscribe((response) => {
       this.columnsData = response;
-      console.log(this.columnsData);
     });
   }
 }
