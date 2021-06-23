3142b727de785afea1daf84104f15787a8071830
poonkodi <poonkodi@pearlthoughts.com>
2021-06-23 16:22:38 +0530

    terms of use page

--- src/app/pages/termsofuse/termsofuse.module.ts
+++ src/app/pages/termsofuse/termsofuse.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TermsofuseComponent } from './termsofuse.component';
+import { SharedModule } from 'src/app/_shared/shared.module';
 
 
 
@@ -10,6 +11,7 @@ import { TermsofuseComponent } from './termsofuse.component';
   ],
   imports: [
     CommonModule,
+    SharedModule
   ]
 })
 export class TermsofuseModule { }
