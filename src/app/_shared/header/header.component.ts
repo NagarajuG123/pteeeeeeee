8427e297ad0159e1a26aed7fa9e8b8bc90857dcd
karthika <karthika@pearlthoughts.com>
2021-07-06 14:47:48 +0530

    Brand header partially done

--- src/app/_shared/header/header.component.ts
+++ src/app/_shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../_core/services/api.service';
+import { Router, NavigationEnd } from '@angular/router';
 import { CommonService } from '../../_core/services/common.service';
 @Component({
   selector: 'app-header',
@@ -8,16 +9,29 @@ import { CommonService } from '../../_core/services/common.service';
 })
 export class HeaderComponent implements OnInit {
   menu: any = [];
+  brandSlug = '1851';
+  brandTitle!: string;
 
-  constructor(private apiService: ApiService, public common: CommonService) {}
+  constructor(private apiService: ApiService, public common: CommonService,private router:Router) {}
 
   ngOnInit(): void {
-    this.getHeader();
-  }
-
-  getHeader() {
-    this.apiService.getHeader().subscribe((response) => {
-      this.menu = response;
+    this.router.events
+    .subscribe(events => {
+      if (events instanceof NavigationEnd) {
+        this.brandSlug = events.url.split('/')[1];
+        if (this.brandSlug === '' || this.brandSlug.includes('#')) {
+          this.brandSlug = '1851';
+        } else {
+          this.brandSlug = this.brandSlug.replace(/\+/g, '');
+         
+        }
+        this.apiService.getAPI(`${this.brandSlug}/header`).subscribe((response) => {
+          this.menu = response.data;
+        });
+        this.apiService.getAPI(`get-brand-by-slug/${this.brandSlug}`).subscribe((response) => {
+          this.brandTitle = response.name;
+        });
+      }
     });
   }
 }
