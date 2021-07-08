f2bf073f03f89c066568a90a3f2bc43be5fbd6f4
poonkodi <poonkodi@pearlthoughts.com>
2021-07-08 16:04:28 +0530

    available market.

--- src/app/pages/brand/info/info.component.ts
+++ src/app/pages/brand/info/info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService  } from 'src/app/_core/services/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonService } from 'src/app/_core/services/common.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-info',
@@ -13,14 +14,19 @@ export class InfoComponent implements OnInit {
   items: any;
   brandSlug: any;
   categories: any = [];
+  staticContent: any;
   selectedIndex: number = 0;
   inquireForm: any;
   isStory: boolean = false;
   isInfo: boolean = false;
   isBought: boolean = false;
+  isExecutive: boolean = false;
+  isMarket: boolean = false;
   company!: string;
+  geoJson: any;
   constructor(private apiService: ApiService,
-    private commonService: CommonService,private route: ActivatedRoute, private router: Router,) { }
+    private commonService: CommonService,private route: ActivatedRoute, private router: Router,  private httpClient: HttpClient,
+) { }
 
   ngOnInit(): void {
     this.route.paramMap
@@ -45,7 +51,8 @@ export class InfoComponent implements OnInit {
       {name: 'Why I BOUGHT', value: 'why-i-bought'},
       {name: 'EXECUTIVE Q&A', value: 'executive'},
       {name: 'AVAILABLE MARKETS', value: 'available-markets'},
-     ];
+    ];
+    this.staticContent = ['why-i-bought', 'executive', 'available-markets'];
   }
   isVideo(item: any) {
     return this.commonService.isVideo(item);
@@ -65,14 +72,25 @@ export class InfoComponent implements OnInit {
       path = 'brand-latest-stories';
       this.isStory = true;
       this.selectedIndex = 2;
-    }
-    else if (item === 'why-i-bought') {
-      path = 'brand-why-i-bought';
-      this.isBought = true;
-      this.selectedIndex = 3;
+    } else if (this.staticContent.includes(item)) {
+      path = 'brand-static-content';
     }
     this.apiService.getAPI(`${this.brandSlug}/${path}`).subscribe((response) => {
-      this.items = response;
+      this.items = response
+      if (item === 'why-i-bought' && this.items.data.find((o: any) => o.slug === 'why-i-bought')) {
+        this.items = this.items.data.find((o: any) => o.slug === 'why-i-bought');
+        this.isBought = true;
+        this.selectedIndex = 3;
+      } else if (item === 'executive' && this.items.data.find((o: any) => o.slug === 'executive')) {
+        this.items = this.items.data.find((o: any) => o.slug === 'executive');
+        this.isExecutive = true;
+        this.selectedIndex = 4;
+      } else if (item === 'available-markets' && this.items.data.find((o: any) => o.slug === 'available-markets')) {
+        this.items = this.items.data.find((o: any) => o.slug === 'available-markets');
+        this.isMarket = true;
+        this.selectedIndex = 5;
+        
+      }
     });
   }
 
