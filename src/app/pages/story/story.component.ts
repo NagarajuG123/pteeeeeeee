da6930dd7e8650bfc699673f7b62bc41015afcdb
poonkodi <poonkodi@pearlthoughts.com>
2021-07-12 23:43:47 +0530

    skeleton.

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -1,97 +1,128 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ApiService } from 'src/app/_core/services/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { MetaService } from 'src/app/_core/services/meta.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-story',
   templateUrl: './story.component.html',
-  styleUrls: ['./story.component.scss']
+  styleUrls: ['./story.component.scss'],
 })
 export class StoryComponent implements OnInit {
   storyData: any = [];
   adsData: any = [];
   newsData: any = [];
   publication: any;
-  storySlug: any = "";
+  storySlug: any = '';
   slug: any;
   storyId: any;
   customOptions: OwlOptions = {};
-
-  constructor(private apiService: ApiService,
-    private router:Router,
+  isBrowser: boolean = false;
+  scrollbarOptions: any;
+  throttle = 1000;
+  scrollDistance = 3;
+  scrollUpDistance = 2;
+  dataLoading: boolean = false;
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
     private route: ActivatedRoute,
-    private metaService:MetaService) { }
-
-  ngOnInit(): void {
-    this.route.paramMap
-      .subscribe( params => {
-        this.slug = params.get('brandSlug');
-        this.storySlug = params.get('storySlug');
-        this.storyId = this.storySlug.slice(this.storySlug.lastIndexOf('-') + 1, this.storySlug.length);
-        this.getStory();
-        this.getAds();
-        this.getNews();
-        this.getPublication();
-      });
-    //ads config
-    this.customOptions = {
-    loop: false,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-     autoplay: true,
-    navText: [
-      '',
-      '',
-    ],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      400: {
-        items: 1,
-      },
-      740: {
-        items: 1,
-      },
-      940: {
-        items: 1,
-      },
-    },
-    nav: true,
-  };
+    private metaService: MetaService,
+    @Inject(PLATFORM_ID) platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
   }
 
-  getStory() {
-    this.apiService.getAPI(`${this.slug}/story/${this.storyId}`).subscribe((response) => {
-      if (response.data === "") {
-        this.router.navigateByUrl('/404');
-      } else {
-        this.storyData = response.data;
-        this.metaService.setSeo(response.meta);
-      }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.slug = params.get('brandSlug');
+      this.storySlug = params.get('storySlug');
+      this.storyId = this.storySlug.slice(
+        this.storySlug.lastIndexOf('-') + 1,
+        this.storySlug.length
+      );
+      this.getStory();
+      this.getAds();
+      this.getNews();
+      this.getPublication();
     });
+    this.setInit();
+  }
+
+  setInit() {
+    this.scrollbarOptions = {
+      axis: 'y',
+      theme: 'minimal-dark',
+      callbacks: {
+        onTotalScroll: () => {},
+      },
+    };
+    this.customOptions = {
+      loop: false,
+      mouseDrag: false,
+      touchDrag: false,
+      pullDrag: false,
+      dots: false,
+      autoplay: true,
+      navText: ['', ''],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        400: {
+          items: 1,
+        },
+        740: {
+          items: 1,
+        },
+        940: {
+          items: 1,
+        },
+      },
+      nav: true,
+    };
+  }
+  getStory() {
+    this.apiService
+      .getAPI(`${this.slug}/story/${this.storyId}`)
+      .subscribe((response) => {
+        if (response.data === '') {
+          this.router.navigateByUrl('/404');
+        } else {
+          this.storyData = response.data;
+          this.metaService.setSeo(response.meta);
+        }
+      });
   }
   getAds() {
     this.apiService.getAPI(`${this.slug}/ads`).subscribe((result) => {
-       result.data.forEach( (ads: { type: string; }) => {
-            if (ads.type === 'Story Ad' || ads.type === 'story_ad') {
-              this.adsData.push(ads);
-            }
-       });
+      result.data.forEach((ads: { type: string }) => {
+        if (ads.type === 'Story Ad' || ads.type === 'story_ad') {
+          this.adsData.push(ads);
+        }
+      });
     });
   }
   getNews() {
-    this.apiService.getAPI(`${this.slug}/news?limit=10&offset=0`).subscribe((result) => {
-      this.newsData = result.data;
-    });
+    this.apiService
+      .getAPI(`${this.slug}/news?limit=10&offset=0`)
+      .subscribe((result) => {
+        this.newsData = result.data;
+      });
   }
-   getPublication() {
-    this.apiService.getAPI(`1851/publication-instance`).subscribe((response ) =>{
-      this.publication = response;
-    });
+  getPublication() {
+    this.apiService
+      .getAPI(`1851/publication-instance`)
+      .subscribe((response) => {
+        this.publication = response;
+      });
+  }
+  onScrollDown() {
+    if (!this.dataLoading) {
+      this.dataLoading = true;
+      // this.scrollEvent.next(true);
+    }
   }
 }
--- src/app/pages/story/story.module.ts
+++ src/app/pages/story/story.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 import { StoryComponent } from './story.component';
 import { ItemComponent } from './item/item.component';
 import { SharedModule } from 'src/app/_shared/shared.module';
-
-
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
-  declarations: [
-    StoryComponent,
-    ItemComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+  declarations: [StoryComponent, ItemComponent],
+  imports: [CommonModule, SharedModule, InfiniteScrollModule],
 })
-export class StoryModule { }
+export class StoryModule {}
