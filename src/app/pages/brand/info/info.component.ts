f5e9c98d42c7af90f307d7582927731507d7fe27
poonkodi <poonkodi@pearlthoughts.com>
2021-07-12 16:27:46 +0530

    available markets.

--- src/app/pages/brand/info/info.component.ts
+++ src/app/pages/brand/info/info.component.ts
@@ -7,7 +7,7 @@ import { MetaService } from 'src/app/_core/services/meta.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidationService } from 'src/app/_core/services/validation.service';
 import { isPlatformBrowser } from '@angular/common';
-
+import * as d3 from 'd3';
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
@@ -45,7 +45,8 @@ export class InfoComponent implements OnInit {
     private router: Router,
     private metaService: MetaService,
     fb: FormBuilder,
-    @Inject(PLATFORM_ID) platformId: Object
+    @Inject(PLATFORM_ID) platformId: Object,
+    private httpClient: HttpClient
   ) {
     this.isBrowser = isPlatformBrowser(platformId);
 
@@ -168,7 +169,7 @@ export class InfoComponent implements OnInit {
       .getAPI(`${this.brandSlug}/${path}`)
       .subscribe((response) => {
         this.items = response;
-        this.metaService.setSeo(response.meta);
+
         if (
           item === 'why-i-bought' &&
           this.items.data.find((o: any) => o.slug === 'why-i-bought')
@@ -176,6 +177,7 @@ export class InfoComponent implements OnInit {
           this.items = this.items.data.find(
             (o: any) => o.slug === 'why-i-bought'
           );
+          this.metaService.setSeo(this.items.meta);
           this.isBought = true;
           this.selectedIndex = 3;
         } else if (
@@ -183,6 +185,7 @@ export class InfoComponent implements OnInit {
           this.items.data.find((o: any) => o.slug === 'executive')
         ) {
           this.items = this.items.data.find((o: any) => o.slug === 'executive');
+          this.metaService.setSeo(this.items.meta);
           this.isExecutive = true;
           this.selectedIndex = 4;
         } else if (
@@ -192,8 +195,24 @@ export class InfoComponent implements OnInit {
           this.items = this.items.data.find(
             (o: any) => o.slug === 'available-markets'
           );
+          this.metaService.setSeo(this.items.meta);
           this.isMarket = true;
           this.selectedIndex = 5;
+
+          const states = [];
+          this.items['available-markets'].forEach((marketData) => {
+            marketData.countries.forEach((m) => {
+              states.push(m);
+            });
+          });
+          const vm = this;
+          this.httpClient
+            .get('../../../assets/js/us-states.json')
+            .subscribe((json: any) => {
+              this.geoJson = json;
+              vm.drawMap(this.items);
+              window.onresize = function () {};
+            });
         }
       });
   }
@@ -217,5 +236,308 @@ export class InfoComponent implements OnInit {
           this.emailSubMessage = res.message;
         }
       });
+  }
+
+  marketingColor(state, items) {
+    if (items['available-markets'] === null) {
+      return '#bcb29a';
+    }
+    for (let n = 0; n < items['available-markets'].length; n++) {
+      for (let i = 0; i < items['available-markets'][n].countries.length; i++) {
+        if (
+          state.properties.abbr ===
+          items['available-markets'][n].countries[i].iso2_code
+        ) {
+          return items['available-markets'][n].color;
+        }
+      }
+    }
+    return '#bcb29a';
+  }
+  drawMap(items) {
+    const isMobile = window.outerWidth < 600 ? true : false;
+    const vm = this;
+    let gadget_projection,
+      gadget_path,
+      gadget_svg,
+      gadget_g,
+      gadget_states,
+      gadget_labels;
+    gadget_projection = d3
+      .geoAlbersUsa()
+      .scale(isMobile ? 400 : 1000)
+      .translate([0, 0]);
+    gadget_path = d3.geoPath().projection(gadget_projection);
+    gadget_svg = d3.selectAll('#map');
+    if (isMobile) {
+      gadget_svg.attr('width', 345).attr('height', 300);
+    }
+    gadget_g = gadget_svg
+      .append('g')
+      .attr(
+        'transform',
+        isMobile ? 'translate(150, 130)' : 'translate(370, 230)'
+      )
+      .append('g')
+      .attr('class', 'states');
+    gadget_states = gadget_g
+      .selectAll('path')
+      .data(this.geoJson.features)
+      .enter()
+      .append('path')
+      .attr('d', gadget_path)
+      .style('stroke', '#000')
+      .style('stroke-width', '1')
+      .attr('name', function (d) {
+        return 'path-' + d.properties.abbr;
+      })
+      .style('fill', function (d) {
+        return vm.marketingColor(d, items);
+      });
+    gadget_g
+      .selectAll('rect')
+      .data(this.geoJson.features)
+      .enter()
+      .filter(function (d) {
+        if (
+          !isMobile &&
+          (d.properties.abbr === 'MA' ||
+            d.properties.abbr === 'RI' ||
+            d.properties.abbr === 'NJ' ||
+            d.properties.abbr === 'CT' ||
+            d.properties.abbr === 'HI' ||
+            d.properties.abbr === 'NJ' ||
+            d.properties.abbr === 'DE' ||
+            d.properties.abbr === 'MD' ||
+            d.properties.abbr === 'DC' ||
+            d.properties.abbr === 'VT' ||
+            d.properties.abbr === 'NH')
+        ) {
+          return true;
+        }
+        return false;
+      })
+      .append('rect')
+      .attr('transform', function (d) {
+        if (!isMobile && d.properties.abbr === 'HI') {
+          return (
+            'translate(' +
+            (gadget_path.centroid(d)[0] + 30) +
+            ',' +
+            gadget_path.centroid(d)[1] +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'VT') {
+          return (
+            'translate(' +
+            (gadget_path.centroid(d)[0] - 40) +
+            ',' +
+            (gadget_path.centroid(d)[1] - 40) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'NH') {
+          return (
+            'translate(' +
+            (gadget_path.centroid(d)[0] - 40) +
+            ',' +
+            (gadget_path.centroid(d)[1] - 70) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'MA') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 35) +
+            ',' +
+            (gadget_path.centroid(d)[1] - 30) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'RI') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 35) +
+            ',' +
+            (gadget_path.centroid(d)[1] - 15) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'CT') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 35) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 5) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'DE') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 35) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 27) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'MD') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 35) +
+            ',' +
+            gadget_path.centroid(d)[1] +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'DC') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 35) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 45) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'NJ') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 35) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 1) +
+            ')'
+          );
+        }
+        return (
+          'translate(' +
+          gadget_path.centroid(d)[0] +
+          ',' +
+          (gadget_path.centroid(d)[1] - 15) +
+          ')'
+        );
+      })
+      .attr('width', function (d) {
+        return 30;
+      })
+      .attr('height', function (d) {
+        return 20;
+      })
+      .attr('stroke', '#000000')
+      .attr('stroke-width', '2')
+      .style('fill', function (d) {
+        return vm.marketingColor(d, items);
+      });
+    gadget_labels = gadget_g
+      .selectAll('text')
+      .data(this.geoJson.features)
+      .enter()
+      .append('text')
+      .attr('class', 'label')
+      .attr('stroke', '#f2f2f2')
+      .attr('font-size', '12px')
+      .attr('transform', function (d) {
+        if (!isMobile && d.properties.abbr === 'HI') {
+          return (
+            'translate(' +
+            (gadget_path.centroid(d)[0] + 40) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 15) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'VT') {
+          return (
+            'translate(' +
+            (gadget_path.centroid(d)[0] - 32) +
+            ',' +
+            (gadget_path.centroid(d)[1] - 25) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'NH') {
+          return (
+            'translate(' +
+            (gadget_path.centroid(d)[0] - 32) +
+            ',' +
+            (gadget_path.centroid(d)[1] - 55) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'MA') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 27) +
+            ',' +
+            (gadget_path.centroid(d)[1] - 15) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'RI') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 27) +
+            ',' +
+            gadget_path.centroid(d)[1] +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'CT') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 27) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 20) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'DE') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 27) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 42) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'MD') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 27) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 15) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'DC') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 27) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 60) +
+            ')'
+          );
+        }
+        if (!isMobile && d.properties.abbr === 'NJ') {
+          return (
+            'translate(' +
+            ($('#map').width() / 2 - 27) +
+            ',' +
+            (gadget_path.centroid(d)[1] + 16) +
+            ')'
+          );
+        }
+        return 'translate(' + gadget_path.centroid(d) + ')';
+      })
+      .text(function (d) {
+        return d.properties.abbr;
+      })
+      .call(this.getBB);
+  }
+
+  getBB(selection) {
+    selection.each(function (d) {
+      d.bbox = this.getBBox();
+    });
   }
 }
