887e656ef518aed01fd318981ec072744bb8b291
karthika <karthika@pearlthoughts.com>
2021-07-13 16:20:06 +0530

    Brand Sitemap Details

--- src/app/pages/brand/brand-routing.module.ts
+++ src/app/pages/brand/brand-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SitemapComponent } from '../sitemap/sitemap.component';
 import { BrandComponent } from './brand.component';
 import { InfoComponent } from './info/info.component';
 
 const routes: Routes = [
+  {
+    path: 'sitemap',
+    component: SitemapComponent,
+  },
   {
     path: '',
     component: BrandComponent,
--- src/app/pages/sitemap/sitemap.component.ts
+++ src/app/pages/sitemap/sitemap.component.ts
@@ -17,35 +17,38 @@ export class SitemapComponent implements OnInit {
   apiUrl: any;
 
   constructor( private apiService: ApiService,private metaService:MetaService,private route: ActivatedRoute,
-    private router:Router) { }
+    private router:Router) { 
+      this.router.events
+      .subscribe(events => {
+        if (events instanceof NavigationEnd) {
+          this.brandSlug = events.url.split('/')[1];
+          if (this.brandSlug === 'sitemap' || this.brandSlug === '' || this.brandSlug.includes('#')) {
+            this.brandSlug = '1851';
+          } else {
+            this.brandSlug = this.brandSlug.replace(/\+/g, '');
+          }
+        }
+      });
+    }
 
   ngOnInit(): void {
-    this.router.events.subscribe((events) => {
-      if (events instanceof NavigationEnd) {
-        this.brandSlug = events.url.split('/')[1];
-        if (this.brandSlug === '' || this.brandSlug.includes('#')) {
-          this.brandSlug = '1851';
-          this.apiUrl = `sitemap-page`;
-          this.getSitemap();
-        } else {
-          this.apiService
-            .getAPI(`get-brand-by-slug/${this.brandSlug.replace(/\+/g, '')}`)
-            .subscribe((response) => {
-              if (response.status != 404 && response.type === 'brand_page') {
-                this.brandSlug = response.slug;
-                this.apiUrl = `${this.brandSlug}/sitemap-page`;
-                this.getSitemap();
-              }
-            });
-        }
+    this.apiService
+    .getAPI(`get-brand-by-slug/${this.brandSlug.replace(/\+/g, '')}`)
+    .subscribe((response) => {
+      if (response.status != 404 && response.type === 'brand_page') {
+        this.brandSlug = response.slug;
+        this.apiUrl = `${this.brandSlug}/sitemap-page`;
+        this.getSitemap();
       }
-      
     });
+    this.apiUrl = `sitemap-page`;
+    this.getSitemap();
   }
 
   getSitemap(){
     this.apiService.getAPI(`${this.apiUrl}`).subscribe((response ) =>{
       this.sitemap = response;
+      console.log(this.sitemap);
       Object.keys(this.sitemap).forEach((year: any) => {
         const monthData: { month: string; number: any; url: any; }[] = [];
         Object.keys(this.sitemap[year]).forEach(month => {
@@ -63,31 +66,17 @@ export class SitemapComponent implements OnInit {
   });
   }
 
-  // getBrandSitemap(){
-  //   this.apiService.getAPI(`${this.brandSlug}/sitemap-page`).subscribe((response ) =>{
-  //     this.sitemap = response;
-  //     Object.keys(this.sitemap).forEach((year: any) => {
-  //       const monthData: { month: string; number: any; url: any; }[] = [];
-  //       Object.keys(this.sitemap[year]).forEach(month => {
-  //         monthData.push({
-  //           month: month,
-  //           number: this.sitemap[year][month]['number'] ? this.sitemap[year][month]['number'] : 0,
-  //           url: this.sitemap[year][month]['url'] ? this.sitemap[year][month]['url'] : ''
-  //         });
-  //       });
-  //       this.data.push({
-  //         year: year,
-  //         data: monthData
-  //     });
-  //   });
-  // });
-  // }
   getMeta() {
     this.apiService.getAPI(`1851/meta`).subscribe((response) => {
       this.metaService.setSeo(response.data);
       this.apiService.getAPI(`1851/publication-instance`).subscribe((result) => {
-        this.metaService.setTitle(`Sitemap for | ${result.title} | ${result.newsType}`);
-        });
+        if(this.brandSlug === '1851'){
+          this.metaService.setTitle(`Sitemap for | ${result.title} | ${result.newsType}`);
+        }
+        else{
+          this.metaService.setTitle(`Sitemap for | ${this.brandSlug}  ${result.title}  | ${result.newsType}`);
+        }  
+     });
     });
   }
 }
