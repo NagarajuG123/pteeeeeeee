69170641a981500e3c09d36a18641730b78084c6
poonkodi <poonkodi@pearlthoughts.com>
2021-07-15 22:44:57 +0530

    contact form.

--- src/app/pages/contacteditorial/contacteditorial.component.ts
+++ src/app/pages/contacteditorial/contacteditorial.component.ts
@@ -1,4 +1,4 @@
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import {
   AfterViewInit,
   Component,
@@ -6,10 +6,9 @@ import {
   OnInit,
   PLATFORM_ID,
 } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { ApiService } from 'src/app/_core/services/api.service';
 import { MetaService } from 'src/app/_core/services/meta.service';
-import { ValidationService } from 'src/app/_core/services/validation.service';
 @Component({
   selector: 'app-contacteditorial',
   templateUrl: './contacteditorial.component.html',
@@ -17,13 +16,8 @@ import { ValidationService } from 'src/app/_core/services/validation.service';
 })
 export class ContacteditorialComponent implements OnInit, AfterViewInit {
   data: any = [];
-  slug = '1851';
-  publication: any;
-  submitErrMsg: string;
-  submitSuccessMsg: string;
-  isSubmitted: boolean = false;
   isBrowser: boolean;
-  contactForm: FormGroup;
+  isServer: boolean;
 
   constructor(
     private apiService: ApiService,
@@ -32,43 +26,17 @@ export class ContacteditorialComponent implements OnInit, AfterViewInit {
     fb: FormBuilder
   ) {
     this.isBrowser = isPlatformBrowser(platformId);
-    this.contactForm = fb.group({
-      first_name: ['', Validators.compose([Validators.required])],
-      last_name: ['', Validators.compose([Validators.required])],
-      email: [
-        '',
-        Validators.compose([
-          Validators.required,
-          ValidationService.emailValidator,
-        ]),
-      ],
-      message: [''],
-      currentFranchisee: [''],
-      currentFranchiseeOwnsUp10units: [''],
-      currentFranchiseeOwns10Plusunits: [''],
-      prospectiveFranchisee: [''],
-      prospectiveFranchiseeUnder6Months: [''],
-      prospectiveFranchiseeOver6Months: [''],
-      franchiseExecutive: [''],
-      franchiseExecutiveLeadership: [''],
-      franchiseNonExecutiveLeadership: [''],
-      businessExecutive: [''],
-      businessExecutiveLeadership: [''],
-      businessNonExecutiveLeadership: [''],
-      other: [''],
-    });
+    this.isServer = isPlatformServer(platformId);
   }
 
   ngOnInit(): void {
     this.getContactEditorial();
     this.getMeta();
-    this.getPublication();
   }
   ngAfterViewInit() {
     if (this.isBrowser) {
       $(document).ready(function () {
         $('.text-field').click(function (e) {
-          console.log('ddf');
           $(this).closest('ul>li').find('.editors-details').slideToggle();
           $(this).closest('ul> li').find('.form-group').addClass('active');
           $(this)
@@ -86,19 +54,11 @@ export class ContacteditorialComponent implements OnInit, AfterViewInit {
     }
   }
   getContactEditorial() {
-    this.apiService
-      .getAPI(`${this.slug}/contact-editorial`)
-      .subscribe((response) => {
-        this.data = response.data;
-      });
+    this.apiService.getAPI('1851/contact-editorial').subscribe((response) => {
+      this.data = response.data;
+    });
   }
-  getPublication() {
-    this.apiService
-      .getAPI(`1851/publication-instance`)
-      .subscribe((response) => {
-        this.publication = response;
-      });
-  }
+
   getMeta() {
     this.apiService.getAPI(`1851/meta`).subscribe((response) => {
       this.metaService.setSeo(response.data);
@@ -108,132 +68,5 @@ export class ContacteditorialComponent implements OnInit, AfterViewInit {
           this.metaService.setTitle(`Contact Editorial | ${result.title}`);
         });
     });
-  }
-
-  toggleCurrent(e) {
-    if (!e.target.checked) {
-      this.contactForm.controls['currentFranchisee'].setValue('');
-      this.contactForm.controls['currentFranchiseeOwnsUp10units'].setValue('');
-      this.contactForm.controls['currentFranchiseeOwns10Plusunits'].setValue(
-        ''
-      );
-    }
-    return;
-  }
-
-  toggleProspective(e) {
-    if (!e.target.checked) {
-      this.contactForm.controls['prospectiveFranchisee'].setValue('');
-      this.contactForm.controls['prospectiveFranchiseeUnder6Months'].setValue(
-        ''
-      );
-      this.contactForm.controls['prospectiveFranchiseeOver6Months'].setValue(
-        ''
-      );
-    }
-    return;
-  }
-
-  toggleExecutive(e) {
-    if (!e.target.checked) {
-      this.contactForm.controls['franchiseExecutive'].setValue('');
-      this.contactForm.controls['franchiseExecutiveLeadership'].setValue('');
-      this.contactForm.controls['franchiseNonExecutiveLeadership'].setValue('');
-    }
-    return;
-  }
-
-  toggleExecutive1(e) {
-    if (!e.target.checked) {
-      this.contactForm.controls['businessExecutive'].setValue('');
-      this.contactForm.controls['businessExecutiveLeadership'].setValue('');
-      this.contactForm.controls['businessNonExecutiveLeadership'].setValue('');
-    }
-    return;
-  }
-  toggleOther(e) {
-    if (!e.target.checked) {
-      this.contactForm.controls['other'].setValue('');
-    }
-    return;
-  }
-  onContactSubmit(contactForm: FormGroup) {
-    this.submitErrMsg = '';
-    this.submitSuccessMsg = '';
-    if (!contactForm.valid) {
-      return;
-    }
-    this.isSubmitted = true;
-    const subscribe_form = {
-      email: this.contactForm.controls['email'].value,
-      first_name: this.contactForm.controls['first_name'].value,
-      last_name: this.contactForm.controls['last_name'].value,
-      message: this.contactForm.controls['message'].value,
-      currentFranchisee: this.contactForm.controls['currentFranchisee'].value
-        ? 1
-        : 0,
-      currentFranchiseeOwnsUp10units: this.contactForm.controls[
-        'currentFranchiseeOwnsUp10units'
-      ].value
-        ? 1
-        : 0,
-      currentFranchiseeOwns10Plusunits: this.contactForm.controls[
-        'currentFranchiseeOwns10Plusunits'
-      ].value
-        ? 1
-        : 0,
-      prospectiveFranchisee: this.contactForm.controls['prospectiveFranchisee']
-        .value
-        ? 1
-        : 0,
-      prospectiveFranchiseeUnder6Months: this.contactForm.controls[
-        'prospectiveFranchiseeUnder6Months'
-      ].value
-        ? 1
-        : 0,
-      prospectiveFranchiseeOver6Months: this.contactForm.controls[
-        'prospectiveFranchiseeOver6Months'
-      ].value
-        ? 1
-        : 0,
-      franchiseExecutive: this.contactForm.controls['franchiseExecutive'].value
-        ? 1
-        : 0,
-      franchiseExecutiveLeadership: this.contactForm.controls[
-        'franchiseExecutiveLeadership'
-      ].value
-        ? 1
-        : 0,
-      franchiseNonExecutiveLeadership: this.contactForm.controls[
-        'franchiseNonExecutiveLeadership'
-      ].value
-        ? 1
-        : 0,
-      businessExecutive: this.contactForm.controls['businessExecutive'].value
-        ? 1
-        : 0,
-      businessExecutiveLeadership: this.contactForm.controls[
-        'businessExecutiveLeadership'
-      ].value
-        ? 1
-        : 0,
-      businessNonExecutiveLeadership: this.contactForm.controls[
-        'businessNonExecutiveLeadership'
-      ].value
-        ? 1
-        : 0,
-      other: this.contactForm.controls['other'].value ? 1 : 0,
-    };
-    this.apiService
-      .postAPI('1851/contact-editorial', subscribe_form)
-      .subscribe((result) => {
-        if (typeof result.data !== 'undefined') {
-          $('.editors').hide();
-          $('.sucess-message').show();
-          this.submitSuccessMsg = result.data.message;
-        } else {
-          this.submitErrMsg = result.error.message;
-        }
-      });
   }
 }
--- src/app/pages/contacteditorial/contacteditorial.module.ts
+++ src/app/pages/contacteditorial/contacteditorial.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContacteditorialComponent } from './contacteditorial.component';
 import { SharedModule } from 'src/app/_shared/shared.module';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [ContacteditorialComponent],
+  declarations: [ContacteditorialComponent, FormComponent],
   imports: [CommonModule, SharedModule],
 })
 export class ContacteditorialModule {}
