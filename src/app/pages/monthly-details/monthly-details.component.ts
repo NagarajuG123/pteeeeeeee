2f2c76601a758cf0b22a7fa03a8b7500aaec3bbc
poonkodi <poonkodi@pearlthoughts.com>
2021-07-16 15:32:08 +0530

    fixes details date.

--- src/app/pages/monthly-details/monthly-details.component.ts
+++ src/app/pages/monthly-details/monthly-details.component.ts
@@ -7,7 +7,7 @@ import * as moment from 'moment';
 @Component({
   selector: 'app-monthly-details',
   templateUrl: './monthly-details.component.html',
-  styleUrls: ['./monthly-details.component.scss']
+  styleUrls: ['./monthly-details.component.scss'],
 })
 export class MonthlyDetailsComponent implements OnInit {
   details: any = [];
@@ -17,42 +17,56 @@ export class MonthlyDetailsComponent implements OnInit {
   date!: any;
   id!: any;
   hasMore: boolean = false;
-  constructor(private apiService: ApiService,
+  constructor(
+    private apiService: ApiService,
     private route: ActivatedRoute,
-    private metaService: MetaService) { }
+    private metaService: MetaService
+  ) {}
 
   ngOnInit(): void {
-      this.route.paramMap
-        .subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.year = params.get('year');
       this.month = params.get('month');
       this.date = params.get('date');
       this.id = params.get('id');
+
       const coverDate = new Date(`${this.year}-${this.month}-${this.date}`);
 
-        this.apiService.getAPI(`1851/journal/cover-details/${this.month}/${this.year}/${this.date}/${this.id}?limit=11&offset=0`).subscribe((response) => {
-
-        this.banner['data'] = response.data[0];
-        this.banner['date'] = moment(coverDate).format('MMMM YYYY');
+      this.apiService
+        .getAPI(
+          `1851/journal/cover-details/${this.month}/${this.year}/${this.date}/${this.id}?limit=11&offset=0`
+        )
+        .subscribe((response) => {
+          this.banner['data'] = response.data[0];
+          this.banner['date'] = moment(coverDate).format('MMMM DD, YYYY');
+          console.log(this.banner['date']);
           this.details = response.data.slice(1, 11);
           this.hasMore = response.has_more;
-            this.apiService.getAPI(`1851/meta`).subscribe((response) => {
-              this.metaService.setSeo(response.data);
-              this.apiService.getAPI(`1851/publication-instance`).subscribe((result) => {
-              this.metaService.setTitle(`${this.banner['date']} issues | ${result.title}`);
-                });
-            });
-      });
+          this.apiService.getAPI(`1851/meta`).subscribe((response) => {
+            this.metaService.setSeo(response.data);
+            this.apiService
+              .getAPI(`1851/publication-instance`)
+              .subscribe((result) => {
+                this.metaService.setTitle(
+                  `${this.banner['date']} issues | ${result.title}`
+                );
+              });
+          });
+        });
     });
   }
-   getMoreData() {
-    this.apiService.getAPI(`1851/journal/cover-details/${this.month}/${this.year}/${this.date}/${this.id}?limit=10&offset=${this.details.length + 1}`)
-    .subscribe(result => {
-      this.hasMore = result.has_more;
-      result.data.forEach((element: any) => {
-        this.details.push(element);
+  getMoreData() {
+    this.apiService
+      .getAPI(
+        `1851/journal/cover-details/${this.month}/${this.year}/${this.date}/${
+          this.id
+        }?limit=10&offset=${this.details.length + 1}`
+      )
+      .subscribe((result) => {
+        this.hasMore = result.has_more;
+        result.data.forEach((element: any) => {
+          this.details.push(element);
+        });
       });
-    });
   }
- 
 }
