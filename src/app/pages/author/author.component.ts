35da8937719a1d8024cf55bc3b682d5934db299f
poonkodi <poonkodi@pearlthoughts.com>
2021-07-19 21:39:28 +0530

    fixes author page.

--- src/app/pages/author/author.component.ts
+++ src/app/pages/author/author.component.ts
@@ -28,47 +28,54 @@ export class AuthorComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.authorSlug = params.get('authorSlug');
-      const author =  this.apiService.getAPI(`author/${this.authorSlug}`);
-      const publication =  this.apiService.getAPI(`1851/publication-instance`);
-      const brand =  this.apiService.getAPI(`author/${this.authorSlug}/branded-contents?limit=10&offset=0`);
-      const editorial =  this.apiService.getAPI(`author/${this.authorSlug}/editorials?limit=10&offset=0`);
+      const author = this.apiService.getAPI(`author/${this.authorSlug}`);
+      const publication = this.apiService.getAPI(`1851/publication-instance`);
+      const brand = this.apiService.getAPI(
+        `author/${this.authorSlug}/branded-contents?limit=10&offset=0`
+      );
+      const editorial = this.apiService.getAPI(
+        `author/${this.authorSlug}/editorials?limit=10&offset=0`
+      );
       const footer = this.apiService.getAPI('1851/footer');
 
-      forkJoin([author,publication,brand,editorial,footer]).subscribe(results=>{
-        if (results[0].data  === '') {
-          this.router.navigateByUrl('/404');
-        } 
-        else{
-          this.author = results[0];
-          this.metaService.setSeo(results[0].meta);
-          this.metaService.setTitle(`${results[0].data.first_name} ${results[1].data.last_name} | ${results[1].title}`);
-          this.brandedContents = results[2].data;
-          this.editorials =  results[3].data;
-          this.footer = results[4].data;
-          this.schema = {
-            '@context': 'https://schema.org/',
-            '@type': 'Person',
-            name: `${this.author.data.first_name} ${this.author.data.last_name}`,
-            url: `${environment.appUrl}${this.router.url}`,
-            image: {
-              '@type': 'ImageObject',
-              url: this.getUserAvater(this.author.data.media),
-              width: 279,
-              height: 279,
-            },
-            mainEntityOfPage: {
-              '@type': 'WebPage',
-              '@id': `${environment.appUrl}`,
-            },
-            sameAs: [
-              this.footer['learn-more']['social-media']['fb-url'],
-              this.footer['learn-more']['social-media']['twitter-url'],
-              this.footer['learn-more']['social-media']['instagram-url'],
-              this.footer['learn-more']['social-media']['linkedin-url'],
-            ],
-          };
+      forkJoin([author, publication, brand, editorial, footer]).subscribe(
+        (results) => {
+          if (results[0].data === '') {
+            this.router.navigateByUrl('/404');
+          } else {
+            this.author = results[0];
+            this.metaService.setSeo(results[0].meta);
+            this.metaService.setTitle(
+              `${results[0].data.first_name} ${results[0].data.last_name} | ${results[1].title}`
+            );
+            this.brandedContents = results[2].data;
+            this.editorials = results[3].data;
+            this.footer = results[4].data;
+            this.schema = {
+              '@context': 'https://schema.org/',
+              '@type': 'Person',
+              name: `${this.author.data.first_name} ${this.author.data.last_name}`,
+              url: `${environment.appUrl}${this.router.url}`,
+              image: {
+                '@type': 'ImageObject',
+                url: this.getUserAvater(this.author.data.media),
+                width: 279,
+                height: 279,
+              },
+              mainEntityOfPage: {
+                '@type': 'WebPage',
+                '@id': `${environment.appUrl}`,
+              },
+              sameAs: [
+                this.footer['learn-more']['social-media']['fb-url'],
+                this.footer['learn-more']['social-media']['twitter-url'],
+                this.footer['learn-more']['social-media']['instagram-url'],
+                this.footer['learn-more']['social-media']['linkedin-url'],
+              ],
+            };
+          }
         }
-      });
+      );
     });
   }
   getUserAvater(media: any) {
