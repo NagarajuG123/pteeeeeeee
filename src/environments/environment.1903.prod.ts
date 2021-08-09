5b9940209b48ada63461e6181bbe6068003d7fa8
poonkodi <poonkodi@pearlthoughts.com>
2021-08-09 17:32:28 +0530

    sidebar issue fixed

---
+++ src/environments/environment.1903.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  port: 4100,
+  apiUrl: 'https://api.room1903.com/v1',
+  imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
+  reCaptchaKey: '6Ldr6S0aAAAAAA014vqQCdLZw7RJbOBI2XI2kp4V',
+  appUrl: 'https://room1903.com',
+  fbUrl: 'https://www.facebook.com/Room1903/',
+};
+  
---
+++ src/environments/environment.1903.qa.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  port: 4600,
+  apiUrl: 'https://api.1903.nlasandbox2.info/v1',
+  imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
+  reCaptchaKey: '6Ldr6S0aAAAAAA014vqQCdLZw7RJbOBI2XI2kp4V',
+  appUrl: 'https://1903-v2.nlasandbox2.info',
+  fbUrl: 'https://www.facebook.com/Room1903/',
+};
