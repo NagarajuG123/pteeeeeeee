8f25f264ac566baf6a2b5934b2301242aeb3edb1
poonkodi <poonkodi@pearlthoughts.com>
2021-08-09 12:48:26 +0530

    disable sw.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from './_core/services/common.service';
-import { SwUpdate } from '@angular/service-worker';
-import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +8,7 @@ import { SwPush } from '@angular/service-worker';
 })
 export class AppComponent implements OnInit {
   title = '1851';
-  constructor(
-    public common: CommonService,
-    private swUpdate: SwUpdate,
-    public swPush: SwPush
-  ) {}
+  constructor(public common: CommonService) {}
 
-  public ngOnInit(): void {
-    if (this.swUpdate.isEnabled) {
-      this.swUpdate.available.subscribe((evt) => {
-        // if (confirm('New version available. Load New Version?')) {
-        //   window.location.reload(true);
-        //   }
-        console.log('Site updated', new Date());
-        history.go(0);
-      });
-    }
-  }
+  public ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,6 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MonthlyDetailsModule } from './pages/monthly-details/monthly-details.module';
 import { GoogleAnalyticsService } from './google-analytics.service';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,12 +29,6 @@ import { environment } from '../environments/environment';
     CommonModule,
     BrowserAnimationsModule,
     MonthlyDetailsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
   ],
   providers: [GoogleAnalyticsService],
   bootstrap: [AppComponent],
