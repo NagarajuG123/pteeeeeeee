2591442dc48325795f0ff6452cb3c7ed543f0038
poonkodi <poonkodi@pearlthoughts.com>
2021-08-11 16:46:33 +0530

    changes api.

--- src/environments/environment.1851.qa.ts
+++ src/environments/environment.1851.qa.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   port: 4400,
-  apiUrl: 'https://api.1851franchise.com/v1',
+  apiUrl: 'https://api.1851.nlasandbox2.info/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
   reCaptchaKey: '6LforqAbAAAAALoQuNmvFAdMYbUY-IdF65DxxyeK',
   appUrl: 'https://1851-v2.nlasandbox2.info',
--- src/environments/environment.ee.qa.ts
+++ src/environments/environment.ee.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.estatenvy.com/v1',
+  apiUrl: 'https://api.ee.nlasandbox2.info/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
   reCaptchaKey: '6LforqAbAAAAALoQuNmvFAdMYbUY-IdF65DxxyeK',
   appUrl: 'https://estatenvy.com',
