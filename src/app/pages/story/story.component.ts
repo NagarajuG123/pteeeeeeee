c99f1b7b8abc6b3e7131e6e6c7f2de8d472cb9c2
poonkodi <poonkodi@pearlthoughts.com>
2021-08-11 20:05:14 +0530

    changes same as section.

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -900,13 +900,13 @@ export class StoryComponent implements OnInit {
               width: this.header['logo']['width'],
               height: this.header['logo']['height'],
             },
+            sameAs: [
+              this.footer.data['learn-more']['social-media']['fb-url'],
+              this.footer.data['learn-more']['social-media']['twitter-url'],
+              this.footer.data['learn-more']['social-media']['instagram-url'],
+              this.footer.data['learn-more']['social-media']['linkedin-url'],
+            ],
           },
-          sameAs: [
-            this.footer.data['learn-more']['social-media']['fb-url'],
-            this.footer.data['learn-more']['social-media']['twitter-url'],
-            this.footer.data['learn-more']['social-media']['instagram-url'],
-            this.footer.data['learn-more']['social-media']['linkedin-url'],
-          ],
         };
       } else {
         this.schema = {};
