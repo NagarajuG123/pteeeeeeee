fbad4164b31952906b28d070034ba53aacd0c4c6
poonkodi <poonkodi@pearlthoughts.com>
2021-08-11 22:05:06 +0530

    changes captcha key

--- src/environments/environment.1851.qa.ts
+++ src/environments/environment.1851.qa.ts
@@ -3,7 +3,7 @@ export const environment = {
   port: 4400,
   apiUrl: 'https://api.1851.nlasandbox2.info/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
-  reCaptchaKey: '6LforqAbAAAAALoQuNmvFAdMYbUY-IdF65DxxyeK',
+  reCaptchaKey: '6LforqAbAAAAAO5u6tru-RkUBn8TBYkCSrZqPJvn',
   appUrl: 'https://1851-v2.nlasandbox2.info',
   fbUrl: 'https://www.facebook.com/1851magazine/',
 };
--- src/environments/environment.ee.qa.ts
+++ src/environments/environment.ee.qa.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.ee.nlasandbox2.info/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
-  reCaptchaKey: '6LforqAbAAAAALoQuNmvFAdMYbUY-IdF65DxxyeK',
+  reCaptchaKey: '6LforqAbAAAAAO5u6tru-RkUBn8TBYkCSrZqPJvn',
   appUrl: 'https://estatenvy.com',
   fbUrl: 'https://www.facebook.com/estatenvy/',
   port: 4500,
