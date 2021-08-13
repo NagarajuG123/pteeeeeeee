48ea863dd3896b75d8fa3c2ebfaba17f5ece4f82
poonkodi <poonkodi@pearlthoughts.com>
2021-08-13 19:47:52 +0530

    changes api.

--- src/environments/environment.1903.qa.ts
+++ src/environments/environment.1903.qa.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   port: 4600,
-  apiUrl: 'https://api.1903.nlasandbox2.info/v1',
+  apiUrl: 'https://api.room1903.com/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
   reCaptchaKey: '6LforqAbAAAAAO5u6tru-RkUBn8TBYkCSrZqPJvn',
   appUrl: 'https://1903-v2.nlasandbox2.info',
