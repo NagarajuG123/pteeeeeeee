6a8f1c4b6e19bdfec1ea729ffbfa41344d8104b3
poonkodi <poonkodi@pearlthoughts.com>
2021-08-26 16:17:09 +0530

    Dev api changes

--- src/environments/environment.ee.qa.ts
+++ src/environments/environment.ee.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.estatenvy.com/v1',
+  apiUrl: 'https://api.ee.nlasandbox2.info/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
   reCaptchaKey: '6LforqAbAAAAAO5u6tru-RkUBn8TBYkCSrZqPJvn',
   appUrl: 'https://estatenvy.com',
