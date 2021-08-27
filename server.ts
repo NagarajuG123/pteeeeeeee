8459aacb5a31c39aa7fe39e2627710e1c3b5b8fb
poonkodi <poonkodi@pearlthoughts.com>
2021-08-27 22:26:10 +0530

    adds font awsome.

--- server.ts
+++ server.ts
@@ -8,10 +8,13 @@ import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 import { environment } from 'src/environments/environment';
+import * as compression from 'compression';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
+  server.use(compression());
+
   const distFolder = join(process.cwd(), 'dist/1851/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html'))
     ? 'index.original.html'
