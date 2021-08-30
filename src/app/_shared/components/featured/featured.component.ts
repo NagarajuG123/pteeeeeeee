58d585719a239a53ffaaab5a926774c409dab214
Karthika <Karthika@SenGs-MacBook-Pro.local>
2021-08-30 12:54:02 +0530

    Featured api changes

--- src/app/_shared/components/featured/featured.component.ts
+++ src/app/_shared/components/featured/featured.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, PLATFORM_ID, Inject } from '@angular/core';
 import { ApiService } from 'src/app/_core/services/api.service';
 import { Details } from 'src/app/_core/models/details.model';
 import { makeStateKey, TransferState } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 
 const RESULT_KEY = makeStateKey<any>('featureState');
 
@@ -12,13 +13,23 @@ const RESULT_KEY = makeStateKey<any>('featureState');
 })
 export class FeaturedComponent implements OnInit {
   @Input() apiUrl!: string;
-  constructor(private apiService: ApiService,private tstate: TransferState) {}
 
+  isBrowser: boolean;
   data: Details[] = [];
   news: Details[] = []
   slug: string = '1851';
 
+  constructor(private apiService: ApiService,
+    private tstate: TransferState,
+    @Inject(PLATFORM_ID) private platformId: object,) {
+      this.isBrowser = isPlatformBrowser(platformId);
+    }
+
   ngOnInit(): void {
+    this.getFeatured();
+  }
+
+  getFeatured(){
     if (this.tstate.hasKey(RESULT_KEY)) {
       const featureData = this.tstate.get(RESULT_KEY, {});
       this.data = featureData['data'];
@@ -31,6 +42,7 @@ export class FeaturedComponent implements OnInit {
       .subscribe((response) => {
         featureData['data'] = response.data.slice(0,4);
       });
+
       this.apiService
       .getAPI(`1851/news?limit=10&offset=0`)
       .subscribe((response) => {
