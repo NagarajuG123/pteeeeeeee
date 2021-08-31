c54fd7a3aacc0b72dba8601cae99586f3db36f83
poonkodi <poonkodi@pearlthoughts.com>
2021-08-31 12:09:23 +0530

    Port no for 1851qa 4100

--- src/environments/environment.1851.qa.ts
+++ src/environments/environment.1851.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  port: 4400,
+  port: 4100,
   apiUrl: 'https://api.1851franchise.com/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
   reCaptchaKey: '6LforqAbAAAAAO5u6tru-RkUBn8TBYkCSrZqPJvn',
