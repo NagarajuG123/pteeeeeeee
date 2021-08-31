af2518d047917d48028ec2c6f4e462586a31c0d2
Karthika <Karthika@SenGs-MacBook-Pro.local>
2021-08-31 16:43:25 +0530

    Most popular section added

--- src/app/_shared/components/most-papular/most-papular.component.ts
+++ src/app/_shared/components/most-papular/most-papular.component.ts
@@ -24,7 +24,7 @@ export class MostPapularComponent implements OnInit {
 
   constructor(private apiService: ApiService,
     private tstate: TransferState,
-    @Inject(PLATFORM_ID) private platformId: object) {
+    @Inject(PLATFORM_ID) private platformId: object,) {
       this.isBrowser = isPlatformBrowser(platformId);
     }
 
@@ -53,95 +53,27 @@ export class MostPapularComponent implements OnInit {
     },
     nav: true,
   };
-  list = dummyData;
   
 
   ngOnInit(): void {
+    this.slug = '1851';
     this.getmostPopular();
   }
 
   getmostPopular(){
-    if (this.tstate.hasKey(RESULT_KEY)) {
-      const mostPupular = this.tstate.get(RESULT_KEY, {});
-      this.data = mostPupular['data'];
-    } else {
-      const mostPupular:any = {}
-
-      this.apiService
-      .getAPI(`${this.slug}/trending?limit=4&offset=0`)
-      .pipe(takeUntil(this.onDestroy$))
-      .subscribe((response) => {
-        mostPupular['data'] = response.data;
-      });
-      
-    this.tstate.set(RESULT_KEY, mostPupular);
-   }
+     if(this.tstate.hasKey(RESULT_KEY)){
+       const mostPopular = this.tstate.get(RESULT_KEY,{});
+       this.data = mostPopular['data'];
+     }
+     else{
+       const mostPopular:any = {};
+       this.apiService
+        .getAPI(`${this.slug}/trending?limit=9&offset=0`)
+        .pipe(takeUntil(this.onDestroy$))
+        .subscribe((response) => { 
+          mostPopular['data'] = response.data;
+        });
+        this.tstate.set(RESULT_KEY, mostPopular);
+     }
   }
 }
-
-// Dummy data array
-const dummyData = [
-  {
-    img: '../../../../assets/images/img.jpg',
-    title: 'Title Lorem Ipsum Conset Ent Umi Abudi Adu Sones',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-  {
-    img: '../../../../assets/images/img-1.jpg',
-    title: 'Title Lorem Ipsum Conset Ent Umi Abudi Adu Sones',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-  {
-    img: '../../../../assets/images/img-2.jpg',
-    title: 'Title Lorem Ipsum: Conset Entumi Abudi',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-  {
-    img: '../../../../assets/images/img.jpg',
-    title: 'Title Lorem Ipsum: Conset Entumi Abudi',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-  {
-    img: '../../../../assets/images/img.jpg',
-    title: 'Title Lorem Ipsum: Conset Entumi Abudi',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-  {
-    img: '../../../../assets/images/img.jpg',
-    title: 'Title Lorem Ipsum: Conset Entumi Abudi',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-  {
-    img: '../../../../assets/images/img.jpg',
-    title: 'Title Lorem Ipsum: Conset Entumi Abudi',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-  {
-    img: '../../../../assets/images/img.jpg',
-    title: 'Title Lorem Ipsum: Conset Entumi Abudi',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-  {
-    img: '../../../../assets/images/img.jpg',
-    title: 'Title Lorem Ipsum: Conset Entumi Abudi',
-    detail:
-      'Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididun.A scelerisque purus semper eget..',
-    createdBy: 'Paige Ivy',
-  },
-];
