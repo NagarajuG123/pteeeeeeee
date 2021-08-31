3bc13d5683d0aee9d1c346d9c7f97e186b7c4b00
Karthika <Karthika@SenGs-MacBook-Pro.local>
2021-08-31 15:35:28 +0530

    Removed Fontawesome in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TransferHttpCacheModule } from '@nguniversal/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +17,6 @@ import { SharedModule } from './_shared/shared.module';
     SharedModule,
     HomeModule,
     HttpClientModule,
-    FontAwesomeModule,
     BrowserAnimationsModule,
     BrowserTransferStateModule
   ],
