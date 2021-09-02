ee447f3cbef25a46279532833f9b90035a4ad337
Ajit Singh <toni.panesar16@gmail.com>
2021-09-02 23:34:00 +0530

    working on it E1851-55

--- src/app/_shared/components/editorial-sections/editorial-sections.component.ts
+++ src/app/_shared/components/editorial-sections/editorial-sections.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorialSectionsComponent implements OnInit {
   tabsName = tabsName;
+  noOfTabsShow = 5;
+  activeTab = 1;
   constructor() {}
 
   ngOnInit(): void {}
+
+  prev() {
+    console.log('prev');
+    this.noOfTabsShow -= 1;
+  }
+  next() {
+    console.log('next');
+    this.noOfTabsShow += 1;
+  }
 }
 
 const tabsName = [
