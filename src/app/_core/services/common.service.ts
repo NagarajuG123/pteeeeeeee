076f97ab04361600337005071f9566eb7d73926d
Ajit Singh <toni.panesar16@gmail.com>
2021-09-05 19:36:32 +0530

    editorial sections code uploader E1851-55

--- src/app/_core/services/common.service.ts
+++ src/app/_core/services/common.service.ts
@@ -5,13 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class CommonService {
   public showmenu: boolean = false;
+  public vtabsItem: number = 5;
 
-  constructor() {}
+  constructor() {
+    this.resizeSidebar(window.innerWidth);
+  }
   toggle() {
     this.showmenu = !this.showmenu;
     window.scrollTo(0, 0);
     // if (this.isMenuOpen) {
     //   this.isMenuOpen = !this.isMenuOpen;
     // }
+  }
+
+  resizeSidebar(val: any) {
+    if (val > 992) this.vtabsItem = 5;
+    else if (val < 993 && val > 767) this.vtabsItem = 3;
+    else if (val < 768 && val > 575) this.vtabsItem = 2;
+    else if (val < 576) this.vtabsItem = 1;
+    else this.vtabsItem = 5;
   }
 }
--- src/app/_shared/components/editorial-sections/editorial-sections.component.ts
+++ src/app/_shared/components/editorial-sections/editorial-sections.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostListener } from '@angular/core';
+import { CommonService } from 'src/app/_core/services/common.service';
 @Component({
   selector: 'app-editorial-sections',
   templateUrl: './editorial-sections.component.html',
@@ -7,19 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorialSectionsComponent implements OnInit {
   tabsName = tabsName;
+  tablist = tabsList;
   noOfTabsShow = 5;
   activeTab = 1;
-  constructor() {}
+  skipTab = 0;
+  constructor(public commonService: CommonService) {}
 
   ngOnInit(): void {}
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.commonService.resizeSidebar(event.target.innerWidth);
+  }
+  setActiveTab(val: any) {
+    this.activeTab = val;
+  }
   prev() {
-    console.log('prev');
-    this.noOfTabsShow -= 1;
+    if (this.skipTab > 0) {
+      this.skipTab -= 1;
+    } else this.skipTab = 0;
   }
   next() {
-    console.log('next');
-    this.noOfTabsShow += 1;
+    if (this.skipTab < this.tabsName.length - this.commonService.vtabsItem) {
+      this.skipTab += 1;
+    }
   }
 }
 
@@ -29,4 +40,58 @@ const tabsName = [
   { tabLabel: 'Franchisors' },
   { tabLabel: 'Industry' },
   { tabLabel: 'Columns' },
+  { tabLabel: 'Columns N' },
+];
+
+const tabsList = [
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
+  {
+    title: 'LOREM IPSUM',
+    detail: 'Title Lorem Ipsum: Conset Entumi Abudi',
+    by: 'Lorem, ipsum',
+  },
 ];
