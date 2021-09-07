318dd1c7ee7952a9c16c198983cfd2183f80a093
Ajit Singh <toni.panesar16@gmail.com>
2021-09-07 23:53:53 +0530

    working on articles section E1851-58

--- src/app/pages/brand/brand.component.ts
+++ src/app/pages/brand/brand.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookF,
+  faLinkedinIn,
+  faYoutube,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.scss']
+  styleUrls: ['./brand.component.scss'],
 })
 export class BrandComponent implements OnInit {
+  faFacebookFIcon = faFacebookF;
+  faLinkedinInIcon = faLinkedinIn;
+  faYoutubeIcon = faYoutube;
+  faInstagramIcon = faInstagram;
+  faCaretDown = faCaretDown;
 
-  constructor() { }
+  relatedArticles = [
+    {
+      media: '../../../assets/dummy-images/Rectangle 221.jpg',
+      title: 'Lorem ipsum morbi tristia con flubet o lemase...',
+      by: 'LOREM IPSUM',
+    },
+    {
+      media: '../../../assets/dummy-images/Rectangle 222.jpg',
+      title: 'Lorem ipsum morbi tristia con flubet o lemase...',
+      by: 'LOREM IPSUM',
+    },
+    {
+      media: '../../../assets/dummy-images/Rectangle 223.jpg',
+      title: 'Lorem ipsum morbi tristia con flubet o lemase...',
+      by: 'LOREM IPSUM',
+    },
+    {
+      media: '../../../assets/dummy-images/Rectangle 224.jpg',
+      title: 'Lorem ipsum morbi tristia con flubet o lemase...',
+      by: 'LOREM IPSUM',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
