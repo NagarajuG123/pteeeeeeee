716f5512c5f332abe4bbd70ea13b1a5b2ad1b558
poonkodi <poonkodi@pearlthoughts.com>
2021-09-11 14:43:59 +0530

    Ads trasfer state

--- src/app/_shared/components/advertisement/advertisement.component.ts
+++ src/app/_shared/components/advertisement/advertisement.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input, Inject, PLATFORM_ID } from '@angular/core';
 import { ApiService } from 'src/app/_core/services/api.service';
 import { isPlatformBrowser } from '@angular/common';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+const RESULT_KEY = makeStateKey<any>(`adsState`);
 
 @Component({
   selector: 'app-advertisement',
@@ -14,8 +19,12 @@ export class AdvertisementComponent implements OnInit {
   @Input() pageType = '';
   slideConfig: any;
   isBrowser: boolean = false;
+  private onDestroySubject = new Subject();
+  onDestroy$ = this.onDestroySubject.asObservable();
+  
   constructor(
     private apiService: ApiService,
+    private tstate: TransferState,
     @Inject(PLATFORM_ID) platformId: Object
   ) {
     this.isBrowser = isPlatformBrowser(platformId);
@@ -36,12 +45,23 @@ export class AdvertisementComponent implements OnInit {
   }
 
   getAds() {
-    this.apiService.getAPI(`${this.slug}/ads`).subscribe((result) => {
-      result.data.forEach((ads: { type: string }) => {
-        if (ads.type === this.type) {
-          this.adsData.push(ads);
-        }
-      });
-    });
+    if (this.tstate.hasKey(RESULT_KEY)) {
+      const adsData = this.tstate.get(RESULT_KEY, {});
+      this.adsData = adsData['data'];
+    }
+    else{
+      const adsData = {};
+      this.apiService.getAPI(`${this.slug}/ads`).pipe(takeUntil(this.onDestroy$))
+      .subscribe((response) => {
+        const data = [];
+        response.data.forEach((ads: { type: string }) => {
+          if (ads.type === this.type) {
+            data.push(ads);
+          }
+        });
+        adsData['data'] = data;
+      this.tstate.set(RESULT_KEY, adsData);
+     });  
+    }
   }
 }
