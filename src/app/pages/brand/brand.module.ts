68bcc5ec614992de7b16448737b0e69066f4ce8f
Karthika <Karthika@SenGs-MacBook-Pro.local>
2021-09-13 20:41:06 +0530

    Latest stories data integrated

--- src/app/pages/brand/brand.module.ts
+++ src/app/pages/brand/brand.module.ts
@@ -4,9 +4,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrandRoutingModule } from './brand-routing.module';
 import { BrandComponent } from './brand.component';
 import { InfoComponent } from './info/info.component';
+import { SharedModule } from 'src/app/_shared/shared.module';
 
 @NgModule({
   declarations: [BrandComponent, InfoComponent],
-  imports: [CommonModule, BrandRoutingModule, FontAwesomeModule],
+  imports: [CommonModule, BrandRoutingModule, FontAwesomeModule,SharedModule],
 })
 export class BrandModule {}
