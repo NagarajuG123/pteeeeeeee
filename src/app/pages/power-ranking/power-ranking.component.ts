00814b5dfa9e49857edb991f6c2d95a6cbcfb891
poonkodi <poonkodi@pearlthoughts.com>
2021-09-14 16:28:36 +0530

    powerranking ui.

--- src/app/pages/power-ranking/power-ranking.component.ts
+++ src/app/pages/power-ranking/power-ranking.component.ts
@@ -12,12 +12,11 @@ const RESULT_KEY = makeStateKey<any>('powerRankingState');
 @Component({
   selector: 'app-power-ranking',
   templateUrl: './power-ranking.component.html',
-  styleUrls: ['./power-ranking.component.scss']
+  styleUrls: ['./power-ranking.component.scss'],
 })
-
 export class PowerRankingComponent implements OnInit {
   data: Powerranking[] = [];
-  contents: Brandsrank[] = [];
+  contents: Brandsrank = {};
   metaData: Meta[] = [];
   publication!: string;
   items: Brandsrank[] = [];
@@ -29,37 +28,36 @@ export class PowerRankingComponent implements OnInit {
     private apiService: ApiService,
     private metaService: MetaService,
     private tstate: TransferState
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    if(this.tstate.hasKey(RESULT_KEY)){
-      const powerRankingData:any = this.tstate.get(RESULT_KEY,{});
+    if (this.tstate.hasKey(RESULT_KEY)) {
+      const powerRankingData: any = this.tstate.get(RESULT_KEY, {});
       this.data = powerRankingData['data'];
       this.contents = powerRankingData['contents'];
       this.items = powerRankingData['contents'].brands;
       this.metaData = powerRankingData['meta'];
       this.publication = powerRankingData['publicationTitle'];
-    }
-    else{
-      const powerRankingData:any = {};
+    } else {
+      const powerRankingData: any = {};
       const powerApi = this.apiService.getAPI(`1851/power-ranking`);
-      const publicationApi = this.apiService.getAPI('1851/publication-instance');
+      const publicationApi = this.apiService.getAPI(
+        '1851/publication-instance'
+      );
 
-      forkJoin([powerApi,publicationApi]).pipe(takeUntil(this.onDestroy$))
-      .subscribe((result) => {
-        powerRankingData['data'] = result[0];
-        powerRankingData['contents'] = result[0].data;
-        powerRankingData['meta'] = result[0].meta;
-        powerRankingData['publicationTitle'] = result[1].title;
-        this.metaService.setSeo(powerRankingData['meta']);
-        this.metaService.setTitle(`Power Rankings | Franchise Brands | ${powerRankingData['publicationTitle']}`);
-      });
-      this.tstate.set(RESULT_KEY,powerRankingData);
+      forkJoin([powerApi, publicationApi])
+        .pipe(takeUntil(this.onDestroy$))
+        .subscribe((result) => {
+          powerRankingData['data'] = result[0];
+          powerRankingData['contents'] = result[0].data;
+          powerRankingData['meta'] = result[0].meta;
+          powerRankingData['publicationTitle'] = result[1].title;
+          this.metaService.setSeo(powerRankingData['meta']);
+          this.metaService.setTitle(
+            `Power Rankings | Franchise Brands | ${powerRankingData['publicationTitle']}`
+          );
+        });
+      this.tstate.set(RESULT_KEY, powerRankingData);
     }
   }
-
-  goDetailPage(brandSlug: string, storySlug: any) {
-    return `${brandSlug}/${storySlug}#brand-latest-stories`;
-  }
-
 }
