b393d659203fdafcd8e48f515be3366cf231ab54
Karthika <Karthika@SenGs-MacBook-Pro.local>
2021-09-14 09:03:17 +0530

    Draw map & Brand Details Added

--- src/app/pages/brand/info/info.component.ts
+++ src/app/pages/brand/info/info.component.ts
@@ -173,6 +173,8 @@ export class InfoComponent implements OnInit {
     const publicationApi = this.apiService.getAPI(`1851/publication-instance`);
     forkJoin([itemApi, publicationApi]).subscribe((results) => {
       this.items = results[0].data;
+      console.log(this.items);
+      console.log(this.items.name);
       let metaData = results[0].meta;
       this.metaService.setSeo(metaData);
       this.metaService.setTitle(`${metaData.seo.title} | ${results[1].title}`);
@@ -267,6 +269,293 @@ export class InfoComponent implements OnInit {
     if (this.skipTab < this.categories.length - this.commonService.vtabsItem) {
       this.skipTab += 1;
     }
+  }
+
+  drawMap(items:any) {
+    // const isMobile = window.outerWidth < 600 ? true : false;
+    // const vm = this;
+    // let gadget_projection,
+    //   gadget_path:any,
+    //   gadget_svg,
+    //   gadget_g,
+    //   gadget_states,
+    //   gadget_labels;
+    // gadget_projection = d3
+    //   .geoAlbersUsa()
+    //   .scale(isMobile ? 400 : 1000)
+    //   .translate([0, 0]);
+    // gadget_path = d3.geoPath().projection(gadget_projection);
+    // gadget_svg = d3.selectAll('#map');
+    // if (isMobile) {
+    //   gadget_svg.attr('width', 345).attr('height', 300);
+    // }
+    // gadget_g = gadget_svg
+    //   .append('g')
+    //   .attr(
+    //     'transform',
+    //     isMobile ? 'translate(150, 130)' : 'translate(370, 230)'
+    //   )
+    //   .append('g')
+    //   .attr('class', 'states');
+    // gadget_states = gadget_g
+    //   .selectAll('path')
+    //   .data(this.geoJson.features)
+    //   .enter()
+    //   .append('path')
+    //   .attr('d', gadget_path)
+    //   .style('stroke', '#000')
+    //   .style('stroke-width', '1')
+    //   .attr('name', function (d:any) {
+    //     return 'path-' + d.properties.abbr;
+    //   })
+    //   .style('fill', function (d) {
+    //     return vm.marketingColor(d, items);
+    //   });
+    // gadget_g
+    //   .selectAll('rect')
+    //   .data(this.geoJson.features)
+    //   .enter()
+    //   .filter(function (d:any) {
+    //     if (
+    //       !isMobile &&
+    //       (d.properties.abbr === 'MA' ||
+    //         d.properties.abbr === 'RI' ||
+    //         d.properties.abbr === 'NJ' ||
+    //         d.properties.abbr === 'CT' ||
+    //         d.properties.abbr === 'HI' ||
+    //         d.properties.abbr === 'NJ' ||
+    //         d.properties.abbr === 'DE' ||
+    //         d.properties.abbr === 'MD' ||
+    //         d.properties.abbr === 'DC' ||
+    //         d.properties.abbr === 'VT' ||
+    //         d.properties.abbr === 'NH')
+    //     ) {
+    //       return true;
+    //     }
+    //     return false;
+    //   })
+    //   .append('rect')
+    //   .attr('transform', function (d:any) {
+    //     if (!isMobile && d.properties.abbr === 'HI') {
+    //       return (
+    //         'translate(' +
+    //         (gadget_path.centroid(d)[0] + 30) +
+    //         ',' +
+    //         gadget_path.centroid(d)[1] +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'VT') {
+    //       return (
+    //         'translate(' +
+    //         (gadget_path.centroid(d)[0] - 40) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] - 40) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'NH') {
+    //       return (
+    //         'translate(' +
+    //         (gadget_path.centroid(d)[0] - 40) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] - 70) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'MA') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 35) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] - 30) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'RI') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 35) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] - 15) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'CT') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 35) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 5) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'DE') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 35) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 27) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'MD') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 35) +
+    //         ',' +
+    //         gadget_path.centroid(d)[1] +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'DC') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 35) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 45) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'NJ') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 35) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 1) +
+    //         ')'
+    //       );
+    //     }
+    //     return (
+    //       'translate(' +
+    //       gadget_path.centroid(d)[0] +
+    //       ',' +
+    //       (gadget_path.centroid(d)[1] - 15) +
+    //       ')'
+    //     );
+    //   })
+    //   .attr('width', function (d:any) {
+    //     return 30;
+    //   })
+    //   .attr('height', function (d:any) {
+    //     return 20;
+    //   })
+    //   .attr('stroke', '#000000')
+    //   .attr('stroke-width', '2')
+    //   .style('fill', function (d:any) {
+    //     return vm.marketingColor(d, items);
+    //   });
+    // gadget_labels = gadget_g
+    //   .selectAll('text')
+    //   .data(this.geoJson.features)
+    //   .enter()
+    //   .append('text')
+    //   .attr('class', 'label')
+    //   .attr('stroke', '#f2f2f2')
+    //   .attr('font-size', '12px')
+    //   .attr('transform', function (d:any) {
+    //     if (!isMobile && d.properties.abbr === 'HI') {
+    //       return (
+    //         'translate(' +
+    //         (gadget_path.centroid(d)[0] + 40) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 15) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'VT') {
+    //       return (
+    //         'translate(' +
+    //         (gadget_path.centroid(d)[0] - 32) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] - 25) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'NH') {
+    //       return (
+    //         'translate(' +
+    //         (gadget_path.centroid(d)[0] - 32) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] - 55) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'MA') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 27) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] - 15) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'RI') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 27) +
+    //         ',' +
+    //         gadget_path.centroid(d)[1] +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'CT') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 27) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 20) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'DE') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 27) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 42) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'MD') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 27) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 15) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'DC') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 27) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 60) +
+    //         ')'
+    //       );
+    //     }
+    //     if (!isMobile && d.properties.abbr === 'NJ') {
+    //       return (
+    //         'translate(' +
+    //         ($('#map').width() / 2 - 27) +
+    //         ',' +
+    //         (gadget_path.centroid(d)[1] + 16) +
+    //         ')'
+    //       );
+    //     }
+    //     return 'translate(' + gadget_path.centroid(d) + ')';
+    //   })
+    //   .text(function (d:any) {
+    //     return d.properties.abbr;
+    //   })
+    //   .call(this.getBB);
+  }
+
+  getBB(selection:any) {
+    selection.each( (d:any) => {
+      // d.bbox = this.getBBox();
+    });
   }
 }
 
