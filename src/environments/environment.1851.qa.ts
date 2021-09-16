930ac94ef38563e11c9845dfaddbbca85ced3663
poonkodi <poonkodi@pearlthoughts.com>
2021-09-16 10:56:51 +0530

    change port.

--- src/environments/environment.1851.qa.ts
+++ src/environments/environment.1851.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  port: 4100,
+  port: 4400,
   apiUrl: 'https://api.1851.nlasandbox2.info/v1',
   api2Url: 'https://apiv2.1851.nlasandbox2.info/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
