c4714f1f20f4ad5acf74bca78c56cf130dd67893
poonkodi <poonkodi@pearlthoughts.com>
2021-09-16 23:29:46 +0530

    fixes about us.

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,21 +1,14 @@
 import { isPlatformBrowser } from '@angular/common';
-import {
-  Component,
-  EventEmitter,
-  Inject,
-  OnInit,
-  Output,
-  PLATFORM_ID,
-} from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ToastrService } from 'ngx-toastr';
 import { forkJoin } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 import { ApiService } from 'src/app/_core/services/api.service';
 import { MetaService } from 'src/app/_core/services/meta.service';
 import { ValidationService } from 'src/app/_core/services/validation.service';
 import { environment } from 'src/environments/environment';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about-us',
@@ -23,22 +16,19 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
-  @Output() imageLoaded = new EventEmitter();
-  publication_contents: any = [];
-  loadedImageNum = 0;
+  faAngleRight = faAngleRight;
+  siteKey: string = environment.reCaptchaKey;
 
-  bannerImageLoaded: Boolean = false;
-  mainimageLoaded: Boolean = false;
+  publicationContents: any = [];
   publication: any = [];
   contactForm: FormGroup;
   isSubmitted: boolean = false;
-  siteKey: string;
-  submitErrMsg: string;
-  submitSuccessMsg: string;
+  submitErrMsg: string = '';
+  submitSuccessMsg: string = '';
   isBrowser: boolean;
   data: any = [];
-  showVideo: Boolean = false;
   videoUrl;
+
   constructor(
     private apiService: ApiService,
     private metaService: MetaService,
@@ -64,51 +54,36 @@ export class AboutUsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.siteKey = environment.reCaptchaKey;
-
     const aboutus = this.apiService.getAPI(`1851/about-us`);
     const meta = this.apiService.getAPI(`1851/meta`);
     const publication = this.apiService.getAPI(`1851/publication-instance`);
     forkJoin([publication, aboutus, meta]).subscribe((results) => {
       this.data = results[1].data;
-      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-        this.data.media.url
-      );
+      this.videoUrl = this.data.media.url;
 
       this.publication = results[0];
       if (this.data?.contents?.length > 1) {
         for (let i = 1; i < this.data.contents.length; i++) {
-          this.publication_contents.push(this.data.contents[i]);
+          this.publicationContents.push(this.data.contents[i]);
         }
       }
       this.metaService.setSeo(results[2].data);
     });
   }
 
-  onModalHide() {
-    this.showVideo = false;
-  }
-
-  bannerImageLoad() {
-    this.bannerImageLoaded = true;
-  }
-  mainImageLoad() {
-    this.mainimageLoaded = true;
-  }
   onContactSubmit(contactForm: FormGroup) {
-    this.submitErrMsg = '';
-    this.submitSuccessMsg = '';
     this.isSubmitted = true;
-
-    // if (!contactForm.valid) {
-    //   return;
-    // }
+    console.log(contactForm.value);
+    if (!contactForm.valid) {
+      return;
+    }
     this.apiService
       .postAPI('1851/about-us', contactForm.value)
       .subscribe((result) => {
+        console.log(result);
         if (typeof result.data !== 'undefined') {
-          this.toastr.success(result.data.message, 'Thanks!');
           this.isSubmitted = false;
+          this.submitSuccessMsg = result.data.message;
           this.resetForm();
         } else {
           this.submitErrMsg = result.error.message;
@@ -125,10 +100,4 @@ export class AboutUsComponent implements OnInit {
     });
   }
   resolved(event) {}
-
-  handleReset() {}
-
-  handleExpire() {}
-
-  handleLoad() {}
 }
