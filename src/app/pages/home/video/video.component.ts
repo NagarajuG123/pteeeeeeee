676fdcee74b49edb1cce28123fee199dfcc1e48e
Karthika <Karthika@SenGs-MacBook-Pro.local>
2021-09-18 14:03:54 +0530

    Video data integrated

--- src/app/pages/home/video/video.component.ts
+++ src/app/pages/home/video/video.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Details } from 'src/app/_core/models/details.model';
+import { ApiService } from 'src/app/_core/services/api.service';
 
 @Component({
   selector: 'app-video',
@@ -7,7 +12,15 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./video.component.scss'],
 })
 export class VideoComponent implements OnInit {
-  constructor() {}
+  videoData: Details[] = [];
+  videoUrl: string;
+  openVideoPlayer = false;
+  private onDestroySubject = new Subject();
+  onDestroy$ = this.onDestroySubject.asObservable();
+  constructor(
+    private state: TransferState,
+    private apiService: ApiService
+  ) {}
   customOptions: OwlOptions = {
     autoplay: true,
     loop: true,
@@ -34,5 +47,17 @@ export class VideoComponent implements OnInit {
     },
     nav: true,
   };
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const VIDEO_KEY = makeStateKey<any>('videoState');
+    const videoData = this.state.get(VIDEO_KEY,null as any);
+    if(!videoData){
+      this.apiService.getAPI(`1851/videos?site=1851`).pipe(takeUntil(this.onDestroy$))
+      .subscribe((result) => {
+        videoData['data'] = result.data;
+      });
+      this.state.set(VIDEO_KEY,videoData as any);
+    } else{
+      this.videoData = videoData['data'];
+    }
+  }
 }
