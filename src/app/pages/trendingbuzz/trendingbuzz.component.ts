1d61ab18410d8f837c5ca0fdb1ceb412bd9ffed9
poonkodi <poonkodi@pearlthoughts.com>
2021-10-12 12:35:02 +0530

    trending brand buzz.

--- src/app/pages/trendingbuzz/trendingbuzz.component.ts
+++ src/app/pages/trendingbuzz/trendingbuzz.component.ts
@@ -8,36 +8,47 @@ import { CommonService } from 'src/app/_core/services/common.service';
 @Component({
   selector: 'app-trendingbuzz',
   templateUrl: './trendingbuzz.component.html',
-  styleUrls: ['./trendingbuzz.component.scss']
+  styleUrls: ['./trendingbuzz.component.scss'],
 })
 export class TrendingbuzzComponent implements OnInit {
   trendingData: Details[] = [];
-  slug: string ='1851';
+  slug: string = '1851';
   hasMore: boolean = false;
   faAngleRight = faAngleRight;
-  constructor( private apiService : ApiService,private metaService: MetaService, public commonService: CommonService) { }
+  constructor(
+    private apiService: ApiService,
+    private metaService: MetaService,
+    public commonService: CommonService
+  ) {}
 
   ngOnInit(): void {
     this.getTrending();
   }
 
-  getTrending(){
-    this.apiService.getAPI(`${this.slug}/trending-buzz?limit=8&offset=0`).subscribe((response ) =>{
-      this.metaService.setSeo(response.data[0].meta);
-      this.trendingData = response.data;
-      this.hasMore = response.has_more;
-      this.apiService.getAPI(`1851/publication-instance`).subscribe((result) => {
-        this.metaService.setTitle(`Trending Brand Buzz | ${result.title}`);
+  getTrending() {
+    this.apiService
+      .getAPI(`${this.slug}/trending-buzz?limit=8&offset=0`)
+      .subscribe((response) => {
+        this.metaService.setSeo(response.data[0].meta);
+        this.trendingData = response.data;
+        this.hasMore = response.has_more;
+        this.apiService
+          .getAPI(`1851/publication-instance`)
+          .subscribe((result) => {
+            this.metaService.setTitle(`Trending Brand Buzz | ${result.title}`);
+          });
       });
-    });
   }
-   getMoreData() {
-    this.apiService.getAPI(`${this.slug}/trending-buzz?limit=4&offset=${this.trendingData.length}`)
-    .subscribe(result => {
-      this.hasMore = result.has_more;
-      result.data.forEach((element: any) => {
-        this.trendingData.push(element);
+  getMoreData() {
+    this.apiService
+      .getAPI(
+        `${this.slug}/trending-buzz?limit=4&offset=${this.trendingData.length}`
+      )
+      .subscribe((result) => {
+        this.hasMore = result.has_more;
+        result.data.forEach((element: any) => {
+          this.trendingData.push(element);
+        });
       });
-    });
   }
 }
