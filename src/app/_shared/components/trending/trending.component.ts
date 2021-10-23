746a21e22c8586adedbe763b17013b99b9cc1a8b
poonkodi <poonkodi@pearlthoughts.com>
2021-10-23 15:25:18 +0530

    fixes trending scroll.

--- src/app/_shared/components/trending/trending.component.ts
+++ src/app/_shared/components/trending/trending.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnInit,
+  PLATFORM_ID,
+  ViewChild,
+} from '@angular/core';
 import { forkJoin, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Details } from 'src/app/_core/models/details.model';
@@ -6,6 +14,7 @@ import { ApiService } from 'src/app/_core/services/api.service';
 import { CommonService } from 'src/app/_core/services/common.service';
 import 'lazysizes';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-trending',
@@ -14,16 +23,23 @@ import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 })
 export class TrendingComponent implements OnInit {
   @Input() slug: string;
+  @ViewChild('carouselBtn') carouselBtn: ElementRef;
+
   data: Details[] = [];
   isLoaded: boolean = false;
   faAngleDown = faAngleDown;
   faAngleUp = faAngleUp;
+  isBrowser: boolean;
+
   private onDestroySubject = new Subject();
   onDestroy$ = this.onDestroySubject.asObservable();
   constructor(
     private apiService: ApiService,
-    public commonService: CommonService
-  ) {}
+    public commonService: CommonService,
+    @Inject(PLATFORM_ID) platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   ngOnInit(): void {
     const trending = this.apiService.getAPI(
@@ -35,5 +51,12 @@ export class TrendingComponent implements OnInit {
         this.data = results[0].data;
         this.isLoaded = true;
       });
+  }
+  ngAfterViewInit() {
+    if (this.isBrowser) {
+      setInterval(() => {
+        this.carouselBtn.nativeElement.click();
+      }, 6000);
+    }
   }
 }
