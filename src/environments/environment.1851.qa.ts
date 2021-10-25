d9e30615ac2e97720210d0d39c2a45576fbbba7a
karthika <karthika@pearlthoughts.com>
2021-10-25 12:30:22 +0530

    env changes

--- src/environments/environment.1851.qa.ts
+++ src/environments/environment.1851.qa.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   port: 4400,
   apiUrl: 'https://api.1851franchise.com/v1',
-  api2Url: 'https://api2.1851.nlasandbox2.info/v1',
+  api2Url: 'https://apiv2.1851.nlasandbox2.info/v1',
   imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
   reCaptchaKey: '6LforqAbAAAAAO5u6tru-RkUBn8TBYkCSrZqPJvn',
   appUrl: 'https://1851-v2.nlasandbox2.info',
