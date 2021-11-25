f408b8641a0075d5820aff8470b852072055f6b2
poonkodi <poonkodi@pearlthoughts.com>
2021-11-25 20:48:34 +0530

    featured series

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,9 +6,10 @@ import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
 import { EditorialSectionsComponent } from './editorial-sections/editorial-sections.component';
+import { SeriesComponent } from './series/series.component';
 
 @NgModule({
-  declarations: [HomeComponent, EditorialSectionsComponent],
+  declarations: [HomeComponent, EditorialSectionsComponent, SeriesComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
   providers: [],
 })
