80b435421dad588ce32906c71d4f81f3ebc585ad
poonkodi <poonkodi@pearlthoughts.com>
2021-11-25 15:14:58 +0530

    removed special feature & video components

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,12 +5,10 @@ import { SharedModule } from '../../_shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
-import { VideoComponent } from './video/video.component';
 import { EditorialSectionsComponent } from './editorial-sections/editorial-sections.component';
-import { SpecialfeatureComponent } from './specialfeature/specialfeature.component';
 
 @NgModule({
-  declarations: [HomeComponent, VideoComponent, EditorialSectionsComponent, SpecialfeatureComponent],
+  declarations: [HomeComponent, EditorialSectionsComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
   providers: [],
 })
