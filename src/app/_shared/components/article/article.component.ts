8907b3608ae7464a7485ecbca67c827f90127c97
poonkodi <poonkodi@pearlthoughts.com>
2021-12-01 19:53:50 +0530

    search issues.

--- src/app/_shared/components/article/article.component.ts
+++ src/app/_shared/components/article/article.component.ts
@@ -4,17 +4,15 @@ import { CommonService } from 'src/app/_core/services/common.service';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent implements OnInit {
   @Input() contents: any;
   @Input() type = '';
   @Input() typeSlug!: string;
+  @Input() class!: string;
 
-  
-  constructor(public commonService: CommonService) { }
-  
-  ngOnInit(): void {
-  }
+  constructor(public commonService: CommonService) {}
 
+  ngOnInit(): void {}
 }
