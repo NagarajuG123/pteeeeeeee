a6483587c424f3bae48adb746988b602bed6b575
poonkodi <poonkodi@pearlthoughts.com>
2021-12-03 18:48:54 +0530

    awards desktop.

--- src/app/pages/home/awards/awards.component.ts
+++ src/app/pages/home/awards/awards.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { Details } from 'src/app/_core/models/details.model';
 import { ApiService } from 'src/app/_core/services/api.service';
+import { CommonService } from 'src/app/_core/services/common.service';
 
 @Component({
   selector: 'app-awards',
@@ -9,13 +12,47 @@ import { ApiService } from 'src/app/_core/services/api.service';
 })
 export class AwardsComponent implements OnInit {
   data: Details[] = [];
-  title: string;
-  constructor(private apiService: ApiService) {}
+  result: string;
+  faAngleRight = faAngleRight;
+  isBrowser: boolean;
+  constructor(
+    private apiService: ApiService,
+    @Inject(PLATFORM_ID) platformId: Object,
+    public commonService: CommonService
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   ngOnInit(): void {
     this.apiService.getAPI(`home-page-featured-content`).subscribe((result) => {
       this.data = result.data.stories;
-      this.title = result.data.title;
+      this.result = result.data;
     });
+  }
+  ngAfterViewInit() {
+    if (this.isBrowser) {
+      const minPerSlide = 5;
+      const parent = document.querySelector('.award-inner');
+      if (parent) {
+        document.querySelectorAll('.award-item').forEach(function (item) {
+          console.log(document.querySelectorAll('.award-item'));
+          let next = item.nextElementSibling;
+          if (!next) {
+            next = parent.querySelector('.carousel-item');
+          }
+          let clone = next.querySelector('div').cloneNode(true);
+          item.appendChild(clone);
+
+          for (var i = 0; i < minPerSlide; i++) {
+            next = next.nextElementSibling;
+            if (!next) {
+              next = parent.querySelector('.carousel-item');
+            }
+            clone = next.querySelector('div').cloneNode(true);
+            item.appendChild(clone);
+          }
+        });
+      }
+    }
   }
 }
