d068d6e7dd2c20a8b557a0bb55d6fffcbe8209da
poonkodi <poonkodi@pearlthoughts.com>
2021-12-17 15:45:07 +0530

    featured carousel section

---
+++ src/app/pages/home/featured-carousel/featured-carousel.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Details } from 'src/app/_core/models/details.model';
+import { ApiService } from 'src/app/_core/services/api.service';
+
+@Component({
+  selector: 'app-featured-carousel',
+  templateUrl: './featured-carousel.component.html',
+  styleUrls: ['./featured-carousel.component.scss']
+})
+export class FeaturedCarouselComponent implements OnInit {
+  brandNews: Details[] = [];
+  customOptions: OwlOptions = {};
+  isLoaded: boolean = false;
+  private onDestroySubject = new Subject();
+  onDestroy$ = this.onDestroySubject.asObservable();
+
+  constructor(private apiService: ApiService,) { }
+
+  ngOnInit(): void {
+    this.apiService
+    .getAPI(`1851/latest?limit=4&offset=0`)
+      .pipe(takeUntil(this.onDestroy$))
+        .subscribe((result) => {
+          this.brandNews = result.data;
+          this.isLoaded = true;
+        }); 
+    this.customOptions = {
+      loop: true,
+      mouseDrag: false,
+      touchDrag: false,
+      pullDrag: false,
+      dots: false,
+      navSpeed: 700,
+      nav: true,
+
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        400: {
+          items: 1,
+        },
+        740: {
+          items: 4,
+        },
+        940: {
+          items: 5,
+        },
+      },
+    };
+  }
+
+  ngOnDestroy() {
+    this.onDestroySubject.next(true);
+    this.onDestroySubject.complete();
+  }
+}
