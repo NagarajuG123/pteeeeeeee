8257234d15b97c53459b69e2e6d0a8d9b0b93350
poonkodi <poonkodi@pearlthoughts.com>
2021-12-31 11:03:45 +0530

    fixes variables.

---
+++ src/environments/environment.sc.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  port: 4000,
+  apiUrl: 'https://api.1851franchise.com/v1',
+  api2Url: 'https://api2.1851franchise.com/v1',
+  s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
+  imageResizeUrl: 'https://dze0oudb6zz9z.cloudfront.net',
+  reCaptchaKey: '6Ldr6S0aAAAAAA014vqQCdLZw7RJbOBI2XI2kp4V',
+  appUrl: 'https://1851franchise.com',
+  fbUrl: 'https://www.facebook.com/1851magazine/',
+  enableSW: false,
+  env: 'production',
+  sentryDsn:
+    'https://e9ff1121f65c4c23aa8562431a68e558@o1063486.ingest.sentry.io/6054469',
+};
