94dc0ac03e39395cc66bc7989d1eb21c002ecd62
poonkodi <poonkodi@pearlthoughts.com>
2022-01-20 15:13:42 +0530

    fix deprecation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
 
   public ngOnInit(): void {
     if (this.swUpdate.isEnabled) {
-      this.swUpdate.available.subscribe(() => {
+      this.swUpdate.versionUpdates.subscribe(() => {
         // if (confirm('New version available. Load New Version?')) {
         window.location.reload();
         // }
