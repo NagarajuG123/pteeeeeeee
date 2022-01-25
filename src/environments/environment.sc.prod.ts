88e15953b80a547028c41f09c7954ce7ce094d2c
poonkodi <poonkodi@pearlthoughts.com>
2022-01-25 15:54:13 +0530

    sc changes.

--- src/environments/environment.sc.prod.ts
+++ src/environments/environment.sc.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   port: 4000,
-  apiUrl: 'https://api.1851franchise.com/v1',
-  api2Url: 'https://api2.1851franchise.com/v1',
+  apiUrl: 'https://api.stachecow.com/v1',
+  api2Url: 'https://api2.stachecow.com/v1',
   s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
-  s3BucketUrl: "https://1851-dev.s3.amazonaws.com",
+  s3BucketUrl: "https://stachecow-prod.s3.amazonaws.com",
   imageResizeUrl: 'https://d3idlmh4hcsex5.cloudfront.net',
-  reCaptchaKey: '6Ldr6S0aAAAAAA014vqQCdLZw7RJbOBI2XI2kp4V',
-  appUrl: 'https://1851franchise.com',
-  fbUrl: 'https://www.facebook.com/1851magazine/',
-  enableSW: false,
+  reCaptchaKey: '6LdzgjYeAAAAAMJut3Y3nEkAwCJ6Ae5gibY3-6F9',
+  appUrl: 'https://stachecow.com',
+  fbUrl: 'https://www.facebook.com/stachecow/',
+  enableSW: true,
   env: 'production'
 };
