226de2f37994cdef4aa18d0b142418a0649af253
poonkodi <poonkodi@pearlthoughts.com>
2022-01-25 17:02:47 +0530

    changes image proxy.

--- src/environments/environment.sc.prod.ts
+++ src/environments/environment.sc.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   api2Url: 'https://api2.stachecow.com/v1',
   s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
   s3BucketUrl: "https://stachecow-prod.s3.amazonaws.com",
-  imageResizeUrl: 'https://d3idlmh4hcsex5.cloudfront.net',
+  imageResizeUrl: 'https://d13ofr2bv2bm2u.cloudfront.net',
   reCaptchaKey: '6LdzgjYeAAAAAMJut3Y3nEkAwCJ6Ae5gibY3-6F9',
   appUrl: 'https://stachecow.com',
   fbUrl: 'https://www.facebook.com/stachecow/',
