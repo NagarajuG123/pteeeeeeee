41c7ba55bc533b0ff255e4459269e06055838e12
poonkodi <poonkodi@pearlthoughts.com>
2022-02-14 13:21:03 +0530

    fix env.

--- src/environments/environment.sc.qa.ts
+++ src/environments/environment.sc.qa.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   port: 4700,
-  apiUrl: 'https://api.stachecow.nlasanddbox2.info/v1',
-  api2Url: 'https://api2.stachecow.nlasanddbox2.info/v1',
+  apiUrl: 'https://api.stachecow.nlasandbox2.info/v1',
+  api2Url: 'https://api2.stachecow.nlasandbox2.info/v1',
   s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
   s3BucketUrl: "https://stachecow-dev.s3.amazonaws.com",
   imageResizeUrl: 'https://d2x2scmkn9im41.cloudfront.net',
   reCaptchaKey: '6LforqAbAAAAAO5u6tru-RkUBn8TBYkCSrZqPJvn',
-  appUrl: 'https://stachecow.nlasanddbox2.info',
+  appUrl: 'https://stachecow.nlasandbox2.info',
   fbUrl: 'https://www.facebook.com/stachecow/',
   enableSW: true,
   env: 'development'
