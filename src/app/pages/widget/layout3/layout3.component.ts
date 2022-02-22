ee8175a6857382ebfdfb7f3f7265a4d12f8eab7d
poonkodi <poonkodi@pearlthoughts.com>
2022-02-22 17:02:31 +0530

    adds widget ui

---
+++ src/app/pages/widget/layout3/layout3.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Details } from 'src/app/_core/models/details.model';
+import { CommonService } from 'src/app/_core/services/common.service';
+
+@Component({
+  selector: 'app-layout3',
+  templateUrl: './layout3.component.html',
+  styleUrls: ['./layout3.component.scss']
+})
+export class Layout3Component implements OnInit {
+ @Input() type: string;
+ @Input() title: string;
+ @Input() class: string;
+ @Input() stories: Details[] =[];
+  constructor(public commonService: CommonService) { }
+
+  ngOnInit(): void {
+
+  }
+
+}
--- src/app/pages/widget/widget.module.ts
+++ src/app/pages/widget/widget.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { WidgetRoutingModule } from './widget-routing.module';
 import { WidgetComponent } from './widget.component';
 import { SharedModule } from 'src/app/_shared/shared.module';
+import { HomeModule } from '../home/home.module';
+import { Layout3Component } from './layout3/layout3.component';
 
 
 @NgModule({
   declarations: [
-    WidgetComponent
+    WidgetComponent,
+    Layout3Component
   ],
   imports: [
     CommonModule,
     WidgetRoutingModule,
-    SharedModule
+    SharedModule,
+    HomeModule
   ]
 })
 export class WidgetModule { }
