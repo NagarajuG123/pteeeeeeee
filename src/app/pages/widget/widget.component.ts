47fd820d97236e8e2ac8a1c2aa9d1bd92a505948
poonkodi <poonkodi@pearlthoughts.com>
2022-03-01 13:16:04 +0530

    most popular ui

--- src/app/pages/widget/widget.component.ts
+++ src/app/pages/widget/widget.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/_core/services/api.service';
+import { CommonService } from 'src/app/_core/services/common.service';
 
 @Component({
   selector: 'app-widget',
@@ -12,7 +13,7 @@ export class WidgetComponent implements OnInit {
   slug:string;
   data:any;
 
-  constructor(private route: ActivatedRoute,private apiService: ApiService,) {
+  constructor(private route: ActivatedRoute,private apiService: ApiService, public commonService: CommonService,) {
    }
 
   ngOnInit(): void {
