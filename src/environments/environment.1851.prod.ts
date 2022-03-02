9c31e13faed06061fda518b1c855929a2959b0e7
poonkodi <poonkodi@pearlthoughts.com>
2022-03-02 10:34:41 +0530

    add api gateway.

--- src/environments/environment.1851.prod.ts
+++ src/environments/environment.1851.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   port: 4000,
   apiUrl: 'https://api.1851franchise.com/v1',
   api2Url: 'https://api2.1851franchise.com/v1',
-  apiGatewayUrl:'https://api.1851franchise.com/v1',
+  apiGatewayUrl:'https://13ztpkfvl9.execute-api.us-east-1.amazonaws.com/prod',
   s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
   imageResizeUrl: 'https://d3idlmh4hcsex5.cloudfront.net',
   // reCaptchaKey: '6Ldr6S0aAAAAAA014vqQCdLZw7RJbOBI2XI2kp4V',
