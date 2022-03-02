40a6b3b2dc6f54ead7adb7a374533700de09495b
poonkodi <poonkodi@pearlthoughts.com>
2022-03-02 10:42:17 +0530

    change api.

--- src/environments/environment.sc.prod.ts
+++ src/environments/environment.sc.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   port: 4000,
   apiUrl: 'https://api.stachecow.com/v1',
   api2Url: 'https://api2.stachecow.com/v1',
-  apiGatewayUrl:'https://x6ztaum5x9.execute-api.us-east-1.amazonaws.com/prod',
+  apiGatewayUrl:'https://api.stachecow.com/v1',
   s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
   imageResizeUrl: 'https://d13ofr2bv2bm2u.cloudfront.net',
   reCaptchaKey: '6Ldq0FIeAAAAAL0_RblWHQAVyOfABp0CP_i00BGq',
