2a9e04f3e7d860eccb32298286321ec5ccba79e3
poonkodi <poonkodi@pearlthoughts.com>
2022-04-12 15:25:21 +0530

    change ee api.

--- src/environments/environment.1851.prod.ts
+++ src/environments/environment.1851.prod.ts
@@ -6,7 +6,6 @@ export const environment = {
   apiGatewayUrl:'https://13ztpkfvl9.execute-api.us-east-1.amazonaws.com/prod/v1',
   s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
   imageResizeUrl: 'https://d3idlmh4hcsex5.cloudfront.net',
-  // reCaptchaKey: '6Ldr6S0aAAAAAA014vqQCdLZw7RJbOBI2XI2kp4V',
   reCaptchaKey: '6Lev138eAAAAAF7Zxz5sHAZAQEZFqnaqeLrxCYTz',
   appUrl: 'https://1851franchise.com',
   fbUrl: 'https://www.facebook.com/1851magazine/',
--- src/environments/environment.ee.prod.ts
+++ src/environments/environment.ee.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.estatenvy.com/v1',
-  api2Url: 'https://api2.estatenvy.com/v1',
+  api2Url: 'https://lbzl3jsppb.execute-api.us-east-1.amazonaws.com/prod/v1',
+  apiGatewayUrl:'https://32shbuzbv9.execute-api.us-east-1.amazonaws.com/prod/v1',
   s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
-  apiGatewayUrl:'https://32shbuzbv9.execute-api.us-east-1.amazonaws.com/prod ',
   imageResizeUrl: 'https://d1y9xwfspocdum.cloudfront.net',
   reCaptchaKey: '6LcOIIQeAAAAAHxtEwM16m89fONoaYeCU_ofw1Sv',
   appUrl: 'https://estatenvy.com',
