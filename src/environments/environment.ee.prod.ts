86fedad02a39ef90faf8bf0f35865c51ee70e90d
AkashSivakumar-Dev <akash@pearlthoughts.com>
2022-04-12 16:15:52 +0530

    Changed api2 url

--- src/environments/environment.ee.prod.ts
+++ src/environments/environment.ee.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.estatenvy.com/v1',
-  api2Url: 'https://lbzl3jsppb.execute-api.us-east-1.amazonaws.com/prod/v1',
+  api2Url: 'https://api2.estatenvy.com/v1',
   apiGatewayUrl:'https://32shbuzbv9.execute-api.us-east-1.amazonaws.com/prod/v1',
   s3Url: 'https://dmprqkmvewks9.cloudfront.net/',
   imageResizeUrl: 'https://d1y9xwfspocdum.cloudfront.net',
