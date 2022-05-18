0f662680f2e1a63b01e0cd60116218b909ef0381
poonkodi <poonkodi@pearlthoughts.com>
2022-05-18 13:04:10 +0530

    fix share url.

--- src/app/pages/story/story.component.ts
+++ src/app/pages/story/story.component.ts
@@ -104,8 +104,8 @@ hasMore:any;
     this.isAuthorPage = false;
     this.storyIndex = false;
     this.duplicate = false;
-    // this.loadScript('https://cdn.iframe.ly/embed.js?api_key=c14ba928acf9cfecbbb987');
-    // this.loadScript("https://platform-api.sharethis.com/js/sharethis.js#property=5cb792e5de419c001204327a&product=custom-share-buttons");
+    this.loadScript('https://cdn.iframe.ly/embed.js?api_key=c14ba928acf9cfecbbb987');
+    this.loadScript("https://dmprqkmvewks9.cloudfront.net/sharethis.js#property=5cb792e5de419c001204327a&product=custom-share-buttons");
   }
 
   loadScript(url:string) {
