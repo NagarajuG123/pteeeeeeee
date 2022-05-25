144ec12ee7f45b5fc745c1d9fbc4ba9f655ef9ea
poonkodi <poonkodi@pearlthoughts.com>
2022-05-25 13:10:43 +0530

    fixes article compoent video fixes

--- src/app/_shared/components/article/article.component.ts
+++ src/app/_shared/components/article/article.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, Input, OnInit, PLATFORM_ID } from '@angular/core';
 import { CommonService } from 'src/app/_core/services/common.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-article',
@@ -13,7 +15,34 @@ export class ArticleComponent implements OnInit {
   @Input() col!: string;
   @Input() class!: string;
 
-  constructor(public commonService: CommonService) {}
+  s3Url = environment.s3Url;
+  isBrowser: boolean;
+  openVideoPlayer: boolean;
+  url: string;
 
+  constructor(public commonService: CommonService,
+    @Inject(PLATFORM_ID) platformId: Object) {
+      this.isBrowser = isPlatformBrowser(platformId);
+    }
+ 
   ngOnInit(): void {}
+
+  updateVideoUrl(url: string) {
+    this.openVideoPlayer = true;
+    this.url = url;
+    console.log(this.url);
+  }
+  ngAfterViewInit() {
+    if (this.isBrowser) {
+      $(document).ready(function() {
+        var $videoSrc;
+        $('.video-btn').click(function() {
+            $videoSrc = $(this).data("src");
+        });
+        $("#editorialModal").on('hide.bs.modal', function (e) {
+          $("#editorialModal iframe").attr("src", $("#editorialModal iframe").attr("src"));
+      });
+    });
+    }
+  }
 }
