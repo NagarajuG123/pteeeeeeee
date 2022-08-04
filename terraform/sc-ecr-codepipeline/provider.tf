dcc73da0700fb16b454bc998dce41ee551e7a748
AkashSivakumar <akash@pearlthoughts>
2022-08-05 02:53:11 +0530

    Edited codebuild timeout value

--- terraform/sc-ecr-codepipeline/provider.tf
+++ terraform/sc-ecr-codepipeline/provider.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = var.region
   default_tags {
     tags = {
-      ManagedBy = "terraform"
-      Project   = var.ProjectName
-      workspace = terraform.workspace
-      tf_backend_bucket   = var.tf_backend_bucket_name
+      ManagedBy         = "terraform"
+      Project           = var.ProjectName
+      workspace         = terraform.workspace
+      tf_backend_bucket = var.tf_backend_bucket_name
     }
   }
 }
--- terraform/sc-ecr-codepipeline/sc-frontend.tf
+++ terraform/sc-ecr-codepipeline/sc-frontend.tf
@@ -1,14 +1,15 @@
 module "EcrCodepipeline-app" {
-  source             = "github.com/PearlThoughts2/terraform-ecs-codepipeline?ref=main"
-  environment        = var.environment
-  image_name         = "app"
-  build_spec_path    = "buildspec/buildspec-sc-dev.yml"
-  vpc_id             = data.aws_vpc.vpc.id
-  subnet_id          = [data.aws_subnets.private_subnets_id.ids]
-  source_branch_name = var.source_branch_name
-  region             = var.region
-  repo_id            = var.repo_id
-  common_name        = local.common_name
+  source                  = "github.com/PearlThoughts2/terraform-ecs-codepipeline?ref=main"
+  environment             = var.environment
+  image_name              = "app"
+  build_spec_path         = "buildspec/buildspec-sc-dev.yml"
+  vpc_id                  = data.aws_vpc.vpc.id
+  subnet_id               = [data.aws_subnets.private_subnets_id.ids]
+  source_branch_name      = var.source_branch_name
+  region                  = var.region
+  repo_id                 = var.repo_id
+  common_name             = local.common_name
+  codebuild_build_timeout = "15"
 
 
   stages = [
@@ -55,16 +56,17 @@ module "EcrCodepipeline-app" {
 
 
 module "EcrCodepipeline-nginx" {
-  source             = "github.com/PearlThoughts2/terraform-ecs-codepipeline?ref=main"
-  environment        = var.environment
-  image_name         = "nginx"
-  build_spec_path    = "buildspec/buildspec-sc-dev-nginx.yml"
-  vpc_id             = data.aws_vpc.vpc.id
-  subnet_id          = [data.aws_subnets.private_subnets_id.ids]
-  source_branch_name = var.source_branch_name
-  region             = var.region
-  repo_id            = var.repo_id
-  common_name        = local.common_name
+  source                  = "github.com/PearlThoughts2/terraform-ecs-codepipeline?ref=main"
+  environment             = var.environment
+  image_name              = "nginx"
+  build_spec_path         = "buildspec/buildspec-sc-dev-nginx.yml"
+  vpc_id                  = data.aws_vpc.vpc.id
+  subnet_id               = [data.aws_subnets.private_subnets_id.ids]
+  source_branch_name      = var.source_branch_name
+  region                  = var.region
+  repo_id                 = var.repo_id
+  common_name             = local.common_name
+  codebuild_build_timeout = "15"
 
   stages = [
     {
