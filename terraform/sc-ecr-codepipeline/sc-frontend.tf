1880657f1f346b49e9fd6106d35dfdd64cc3165a
AkashSivakumar <akash@pearlthoughts>
2022-08-04 17:49:36 +0530

    Added stachecow ecr-codepipeline IaC

---
+++ terraform/sc-ecr-codepipeline/sc-frontend.tf
@@ -0,0 +1,108 @@
+module "EcrCodepipeline-app" {
+  source             = "github.com/PearlThoughts2/terraform-ecs-codepipeline?ref=main"
+  environment        = var.environment
+  image_name         = "${var.ProjectName}-app"
+  build_spec_path    = "buildspec/buildspec-sc-dev.yml"
+  vpc_id             = data.aws_vpc.vpc.id
+  subnet_id          = [data.aws_subnets.private_subnets_id.ids]
+  source_branch_name = var.source_branch_name
+  region             = var.region
+  repo_id            = var.repo_id
+  common_name        = local.common_name
+
+
+  stages = [
+    {
+      name = "Source"
+      action = [
+        {
+          "name"             = "Source"
+          "category"         = "Source"
+          "owner"            = "AWS"
+          "provider"         = "CodeStarSourceConnection"
+          "version"          = "1"
+          "input_artifacts"  = []
+          "output_artifacts" = ["source_output"]
+
+          "configuration" = {
+            ConnectionArn    = module.EcrCodepipeline-app.codestar_arn
+            FullRepositoryId = var.repo_id
+            BranchName       = var.source_branch_name
+            DetectChanges    = false
+          }
+        }
+      ]
+    },
+    {
+      name = "Build"
+      action = [
+        {
+          "name"             = "Build"
+          "category"         = "Build"
+          "owner"            = "AWS"
+          "version"          = "1"
+          "provider"         = "CodeBuild"
+          "input_artifacts"  = ["source_output"]
+          "output_artifacts" = ["build_output"]
+          "configuration" = {
+            ProjectName = module.EcrCodepipeline-app.codebuild_name
+          }
+        }
+      ]
+    }
+  ]
+}
+
+
+module "EcrCodepipeline-nginx" {
+  source             = "github.com/PearlThoughts2/terraform-ecs-codepipeline?ref=main"
+  environment        = var.environment
+  image_name         = "${var.ProjectName}-nginx"
+  build_spec_path    = "buildspec/buildspec-sc-dev-nginx.yml"
+  vpc_id             = data.aws_vpc.vpc.id
+  subnet_id          = [data.aws_subnets.private_subnets_id.ids]
+  source_branch_name = var.source_branch_name
+  region             = var.region
+  repo_id            = var.repo_id
+  common_name        = local.common_name
+
+  stages = [
+    {
+      name = "Source"
+      action = [
+        {
+          "name"             = "Source"
+          "category"         = "Source"
+          "owner"            = "AWS"
+          "provider"         = "CodeStarSourceConnection"
+          "version"          = "1"
+          "input_artifacts"  = []
+          "output_artifacts" = ["source_output"]
+          "configuration" = {
+            ConnectionArn    = module.EcrCodepipeline-nginx.codestar_arn
+            FullRepositoryId = var.repo_id
+            BranchName       = var.source_branch_name
+            DetectChanges    = false
+          }
+        }
+      ]
+    },
+    {
+      name = "Build"
+      action = [
+        {
+          "name"             = "Build"
+          "category"         = "Build"
+          "owner"            = "AWS"
+          "version"          = "1"
+          "provider"         = "CodeBuild"
+          "input_artifacts"  = ["source_output"]
+          "output_artifacts" = ["build_output"]
+          "configuration" = {
+            ProjectName = module.EcrCodepipeline-nginx.codebuild_name
+          }
+        }
+      ]
+    }
+  ]
+}
---
+++ terraform/sc-ecr-codepipeline/vars.tf
@@ -0,0 +1,11 @@
+variable "environment" {}
+variable "source_branch_name" {}
+variable "ProjectName" {
+  default = "Stachecow"
+}
+variable "region" {
+  default = "us-east-1"
+}
+variable "repo_id" {
+  default = "hellomainland/1851-ui3"
+}
