61eeb48fcc5fdd500d0b4f327986e72501f56081
AkashSivakumar <akash@pearlthoughts>
2022-08-10 15:28:37 +0530

    Added ee tf scripts

--- terraform/ee-ecr-codepipeline/vars.tf
+++ terraform/ee-ecr-codepipeline/vars.tf
@@ -2,7 +2,7 @@ variable "environment" {}
 variable "source_branch_name" {}
 variable "tf_backend_bucket_name" {}
 variable "ProjectName" {
-  default = "1851"
+  default = "ee"
 }
 variable "region" {
   default = "us-east-1"
