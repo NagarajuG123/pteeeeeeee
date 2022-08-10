ca6f70121ac3fea6860fbeacf80e2f6e5b758638
AkashSivakumar <akash@pearlthoughts>
2022-08-10 10:59:10 +0530

    Added 1903 ecr-codepipeline tf scripts

---
+++ terraform/1903-ecr-codepipeline/vars.tf
@@ -0,0 +1,12 @@
+variable "environment" {}
+variable "source_branch_name" {}
+variable "tf_backend_bucket_name" {}
+variable "ProjectName" {
+  default = "e1903"
+}
+variable "region" {
+  default = "us-east-1"
+}
+variable "repo_id" {
+  default = "hellomainland/1851-ui3"
+}
