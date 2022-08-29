848847cbe3cb92fad3c750bf4821eabfbeeec2d5
kirupakaran369 <kirupakaran.contact@gmail.com>
2022-08-29 15:38:14 +0530

    bucket decreased to 2

--- terraform/1851-ecr-codepipeline/backend/dev.tfvars
+++ terraform/1851-ecr-codepipeline/backend/dev.tfvars
@@ -1,3 +1,3 @@
-bucket = "tf-state-keeper-dev"
+bucket = "tf-state-keeper-dev-all"
 key    = "ecr-codepipeline/1851.tfstate"
 region = "us-east-1"
--- terraform/1851-ecr-codepipeline/vars.tf
+++ terraform/1851-ecr-codepipeline/vars.tf
@@ -1,6 +1,12 @@
-variable "environment" {}
-variable "source_branch_name" {}
-variable "tf_backend_bucket_name" {}
+variable "environment" {
+  default = "dev-all"
+}
+variable "source_branch_name" {
+  default = "development"
+}
+variable "tf_backend_bucket_name" {
+  default = "tf-state-keeper-dev-all"
+}
 variable "ProjectName" {
   default = "1851"
 }
@@ -8,8 +14,14 @@ variable "region" {
   default = "us-east-1"
 }
 variable "repo_id" {
-  default = "hellomainland/1851-ui3"
+  default = "pt1851/1851-ui3"
 }
 variable "publication" {
   default = "1851"
+}
+variable "codebuild_bucket" {
+  default = "sc-codebuild-sc"
+}
+variable "codepipeline_bucket" {
+  default = "sc-codepipeline-sc"
 }
